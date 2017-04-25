8b883cf0705b964dcbfe36ab4e8b628c6a64811e
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-04-25 13:30:58 -0700

    parser for new language

--- src/aptomi/slinga/parser.go
+++ src/aptomi/slinga/parser.go
@@ -7,16 +7,25 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+type Allocation struct {
+	Name     	string
+	Criteria	[]string
+	Labels   	map[string]map[string]string
+}
+
 type Context struct {
-	Labels map[string]string
-	Instances []string
+	Name     	string
+	Service     string
+	Criteria	[]string
+	Labels   	map[string]map[string]string
+
+	Allocations []Allocation
 }
 
 type Service struct {
-	Name string
-	Code string
-	Labels map[string]string
-	Contexts map[string]Context
+	Name     	string
+	Code     	string
+	Labels   	map[string]map[string]string
 }
 
 // Load slinga service from file
@@ -33,8 +42,38 @@ func loadService(filename string) Service {
 	return t
 }
 
+// Load slinga service from file
+func loadContext(filename string) Context {
+	dat, e := ioutil.ReadFile(filename)
+	if e != nil {
+		panic(e)
+	}
+	t := Context{}
+	e = yaml.Unmarshal([]byte(dat), &t)
+	if e != nil {
+		log.Fatalf("error: %v", e)
+	}
+	return t
+}
+
 // Dump slinga service onto screen
-func dump(t Service) {
+func dumpService(t Service) {
+	d, e := yaml.Marshal(&t)
+	if e != nil {
+		log.Fatalf("error: %v", e)
+	}
+	fmt.Printf("--- dump:\n%s\n", string(d))
+
+	m := make(map[interface{}]interface{})
+	e = yaml.Unmarshal([]byte(string(d)), &m)
+	if e != nil {
+		log.Fatalf("error: %v", e)
+	}
+	fmt.Printf("%v\n\n", m)
+}
+
+// Dump slinga context onto screen
+func dumpContext(t Context) {
 	d, e := yaml.Marshal(&t)
 	if e != nil {
 		log.Fatalf("error: %v", e)
@@ -50,7 +89,13 @@ func dump(t Service) {
 }
 
 func main() {
-	filename := "/Users/ralekseenkov/go/aptomi-workspace/src/aptomi/slinga/example-1/service.kafka.yaml"
-	t := loadService(filename)
-	dump(t)
+	dir := "/Users/ralekseenkov/go/src/aptomi-workspace/src/aptomi/slinga/example-1/"
+
+	filenameSvc := dir + "service.kafka.yaml"
+	s := loadService(filenameSvc)
+	dumpService(s)
+
+	filenameCtx := dir + "context.test.kafka.yaml"
+	c := loadContext(filenameCtx)
+	dumpContext(c)
 }
