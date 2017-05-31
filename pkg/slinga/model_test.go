f634f8619c7e5d4a6f6a21d13e9f3985716aad3c
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-05-30 20:54:25 -0700

    fixed tests

--- pkg/slinga/model_test.go
+++ pkg/slinga/model_test.go
@@ -27,14 +27,15 @@ func TestLabelOperations(t *testing.T) {
 
 func TestTemplateEvaluation(t *testing.T) {
 	alice := LoadUserByIDFromDir("testdata/unittests", "1")
+	labels := LabelSet{Labels: map[string]string{"tagname": "tagvalue"}}
 
-	result, err := evaluateTemplate("test-{{.User.Labels.team}}", alice)
+	result, err := evaluateTemplate("test-{{.User.Labels.team}}-{{.Labels.tagname}}", alice, labels)
 	assert.Nil(t, err, "Template should evaluate without errors")
-	assert.Equal(t, "test-platform_services", result, "Template should be evaluated correctly, user team parameter must be substituted with its value")
+	assert.Equal(t, "test-platform_services-tagvalue", result, "Template should be evaluated correctly, user team parameter must be substituted with its value")
 
-	result, err = evaluateTemplate("test-{{.User.MissingField}}-{{.MissingObject}}", alice)
+	result, err = evaluateTemplate("test-{{.User.MissingField}}-{{.MissingObject}}", alice, labels)
 	assert.NotNil(t, err, "Template should not evaluate, because there is a missing field")
 
-	result, err = evaluateTemplate("test-{{.User.Labels.missinglabel}}", alice)
+	result, err = evaluateTemplate("test-{{.User.Labels.missinglabel}}", alice, labels)
 	assert.NotNil(t, err, "Template should not evaluate, because there is a missing label")
 }
