c38629faf5d1496d4a6f0afb562c39fc1b23cad1
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-06-14 02:37:09 -0700

    comment updates in demo sync

--- tools/demo-sync.sh
+++ tools/demo-sync.sh
@@ -9,17 +9,17 @@ aptomi policy add users demo/users
 aptomi policy add secrets demo/secrets
 aptomi policy add chart demo/charts
 
-# Add istio service
+# Add service mesh istio service (system-level)
 aptomi policy add service demo/services/istio
 aptomi policy add context demo/services/istio
 
-# Add analytics pipeline service
+# John defines and operates analytics_pipeline service (defines all contexts)
 aptomi policy add service demo/services/analytics_pipeline
 aptomi policy add context demo/services/analytics_pipeline
 
-# Add twitter stats service
+# Frank defines and operates twitter_stats service (defines all contexts)
 aptomi policy add service demo/services/twitter_stats
 aptomi policy add context demo/services/twitter_stats
 
-# Add dependencies
+# Add initial dependency (Frank runs production instance of twitter_stats)
 aptomi policy add dependencies demo/dependencies/dependencies.frank-prod-ts.yaml
