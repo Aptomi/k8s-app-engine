32476a6bd3128e48713703ee1e1d0edaf3e98fe7
Sergey Lukjanov <me@slukjanov.name>
2017-06-14 05:36:15 -0700

    Simplify and fix logging for fork.go

--- pkg/slinga/fork.go
+++ pkg/slinga/fork.go
@@ -1,52 +1,18 @@
 package slinga
 
 import (
-	"bufio"
-	"github.com/golang/glog"
 	"os/exec"
+	"fmt"
 )
 
-func runCmd(cmdName string, cmdArgs ...string) error {
-	cmd := exec.Command(cmdName, cmdArgs...)
-	glog.Infof("Running command '%s' with args: %s", cmdName, cmdArgs)
-
-	cmdStdoutReader, err := cmd.StdoutPipe()
+func runCmd(cmdName string, cmdArgs ...string) (string, error) {
+	out, err := exec.Command(cmdName, cmdArgs...).Output()
 	if err != nil {
-		glog.Errorf("Failed running command '%s' (with args: %s): %s", cmdName, cmdArgs, err)
-		return err
-	}
-
-	cmdStdoutScanner := bufio.NewScanner(cmdStdoutReader)
-	go func() {
-		for cmdStdoutScanner.Scan() {
-			glog.Infof("%s out | %s\n", cmdName, cmdStdoutScanner.Text())
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			return "", fmt.Errorf("Exit err (%s): %s", exitErr.Error(), exitErr.Stderr)
 		}
-	}()
-
-	cmdStderrReader, err := cmd.StderrPipe()
-	if err != nil {
-		glog.Errorf("Failed running command '%s' (with args: %s): %s", cmdName, cmdArgs, err)
-		return err
+		return "", err
 	}
 
-	cmdStderrScanner := bufio.NewScanner(cmdStderrReader)
-	go func() {
-		for cmdStderrScanner.Scan() {
-			glog.Infof("%s err | %s\n", cmdName, cmdStderrScanner.Text())
-		}
-	}()
-
-	err = cmd.Start()
-	if err != nil {
-		glog.Errorf("Failed running command '%s' (with args: %s): %s", cmdName, cmdArgs, err)
-		return err
-	}
-
-	err = cmd.Wait()
-	if err != nil {
-		glog.Errorf("Failed running command '%s' (with args: %s): %s", cmdName, cmdArgs, err)
-		return err
-	}
-
-	return nil
+	return string(out), nil
 }
