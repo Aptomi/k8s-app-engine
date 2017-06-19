aad424403d28700aa3d6e0fc800d6c5d27b71912
Sergey Lukjanov <me@slukjanov.name>
2017-06-18 17:41:54 -0700

    Add simple http server that serves static and dynamic endpoints

---
+++ pkg/slinga/server.go
@@ -0,0 +1,26 @@
+package slinga
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func Serve(host string, port int) {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+	router.ServeFiles("/static/*filepath", http.Dir("static/"))
+
+	fmt.Println("Serving")
+	// todo handle error returned from ListenAndServe (path to Fatal??)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router)
+}
