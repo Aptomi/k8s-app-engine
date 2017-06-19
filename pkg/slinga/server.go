08120faf2e6965dc5e1894a133f4294070f30466
Sergey Lukjanov <me@slukjanov.name>
2017-06-18 19:07:18 -0700

    Dumb endpoints API impl

--- pkg/slinga/server.go
+++ pkg/slinga/server.go
@@ -1,6 +1,7 @@
 package slinga
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
@@ -14,10 +15,21 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
+func Endpoints(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Load the previous usage state
+	state := LoadServiceUsageState()
+
+	endpoints := state.Endpoints()
+
+	res, _ := json.Marshal(endpoints)
+	fmt.Fprint(w, string(res))
+}
+
 func Serve(host string, port int) {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
+	router.GET("/api/endpoints", Endpoints)
 	router.ServeFiles("/static/*filepath", http.Dir("public/static"))
 
 	fmt.Println("Serving")
