63e825da4860ed5f499c01ae255a693a1e2d2aee
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-06-20 16:20:41 -0700

    put policy into db/policy, not into db/policy/demo

--- tools/demo-init.sh
+++ tools/demo-init.sh
@@ -11,7 +11,7 @@ make install
 aptomi policy reset --force
 
 # Sync demo policy
-cp -r ./demo $APTOMI_DB/policy
+cp -r ./demo/ $APTOMI_DB/policy
 
 # Run aptomi policy apply in noop mode
 aptomi policy apply --noop
