9ac0c5e9e5c469abb1d4a6227d964bc75f9e650a
Sergey Lukjanov <me@slukjanov.name>
2017-06-21 10:08:58 -0700

    server: add current user id and services to the details

--- pkg/slinga/visibility/view_detail.go
+++ pkg/slinga/visibility/view_detail.go
@@ -11,13 +11,14 @@ type item struct {
 }
 
 type detail struct {
+	UserId       string
 	Users        []*item
 	Services     []*item
 	Dependencies []*item
 }
 
 func NewDetails(userId string, globalUsers slinga.GlobalUsers, state slinga.ServiceUsageState) detail {
-	r := detail{make([]*item, 0), make([]*item, 0), make([]*item, 0)}
+	r := detail{userId, make([]*item, 0), make([]*item, 0), make([]*item, 0)}
 
 	// Users
 	userIds := make([]string, 0)
@@ -44,8 +45,22 @@ func NewDetails(userId string, globalUsers slinga.GlobalUsers, state slinga.Serv
 
 	sort.Strings(depIds)
 
+	r.Dependencies = append(r.Dependencies, &item{"all", "All"})
 	for _, depId := range depIds {
-		r.Dependencies = append(r.Dependencies, &item{depId, deps[depId].Service})
+		r.Dependencies = append(r.Dependencies, &item{depId, deps[depId].ID})
+	}
+
+	// Services
+	svcIds := make([]string, 0)
+	for svcId := range state.Policy.Services {
+		svcIds = append(svcIds, svcId)
+	}
+
+	sort.Strings(svcIds)
+
+	for _, svcId := range svcIds {
+		// check service owner
+		r.Services = append(r.Services, &item{svcId, state.Policy.Services[svcId].Name})
 	}
 
 	return r
