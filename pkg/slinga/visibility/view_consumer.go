e5ce879a4ca1932acb2297c4839f8c65f37306c0
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-06-20 17:05:46 -0700

    consumer view

--- pkg/slinga/visibility/view_consumer.go
+++ pkg/slinga/visibility/view_consumer.go
@@ -28,49 +28,39 @@ func (view ConsumerView) GetData() graphEntry {
 			// Step 1 - add a node for every dependency found
 			dependencyNode := newDependencyNode(dependency, false)
 			view.g.addNode(dependencyNode, 0)
+
+			// Step 2 - process subgraph
+			if len(dependency.ResolvesTo) > 0 {
+				view.addResolvedDependencies(dependency.ResolvesTo, dependencyNode, 1)
+			}
 		}
 	}
 
 	return view.g.GetData()
 }
 
-/*
-// Adds to the graph nodes/edges which trigger usage of a given service instance
-func (view ConsumerView) addEveryoneWhoUses(serviceKey string, svcInstanceNodePrev graphNode, nextLevel int) {
-	// retrieve service instance
-	instance := view.state.GetResolvedUsage().ComponentInstanceMap[serviceKey]
+// Adds to the graph nodes/edges which are triggered by usage of a given dependency
+func (view ConsumerView) addResolvedDependencies(key string, nodePrev graphNode, nextLevel int) {
+	// retrieve instance
+	service, context, allocation, component := slinga.ParseServiceUsageKey(key)
+	v := view.state.GetResolvedUsage().ComponentInstanceMap[key]
 
-	// if there are no incoming edges, it means we came to the very beginning of the chain (i.e. dependency)
-	if len(instance.EdgesIn) <= 0 {
-		// add nodes for all dependencies
-		for _, dependencyID := range instance.DependencyIds {
-			// add a node for dependency
-			dependencyNode := newDependencyNode(view.state.Dependencies.DependenciesByID[dependencyID])
-			view.g.addNode(dependencyNode, nextLevel)
+	// if it's a service, add node and connext with previous
+	if component == slinga.ComponentRootName {
+		// add service instance node
+		svcInstanceNode := newServiceInstanceNode(key, view.state.Policy.Services[service], context, allocation, v, nextLevel <= 1)
+		view.g.addNode(svcInstanceNode, nextLevel)
 
-			// connect prev service instance node and dependency node
-			view.g.addEdge(svcInstanceNodePrev, dependencyNode)
-		}
-	} else {
-		// go over all incoming edges
-		for k := range instance.EdgesIn {
-			service, context, allocation, component := slinga.ParseServiceUsageKey(k)
-			v := view.state.GetResolvedUsage().ComponentInstanceMap[k]
-			if component == slinga.ComponentRootName {
-				// if it's a service instance, add a node
-				svcInstanceNode := newServiceInstanceNode(k, view.state.Policy.Services[service], context, allocation, v, false)
-				view.g.addNode(svcInstanceNode, nextLevel)
+		// connect service instance nodes
+		view.g.addEdge(nodePrev, svcInstanceNode)
 
-				// connect service instance nodes
-				view.g.addEdge(svcInstanceNodePrev, svcInstanceNode)
+		// update prev
+		nodePrev = svcInstanceNode
+	}
 
-				// proceed further with updated service instance node
-				view.addEveryoneWhoUses(k, svcInstanceNode, nextLevel+1)
-			} else {
-				// proceed further, carry prev service instance node
-				view.addEveryoneWhoUses(k, svcInstanceNodePrev, nextLevel)
-			}
-		}
+	// go over all outgoing edges
+	for k := range v.EdgesOut {
+		// proceed further with updated service instance node
+		view.addResolvedDependencies(k, nodePrev, nextLevel+1)
 	}
 }
-*/
