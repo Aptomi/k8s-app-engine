967d11e9fb3dc0e3bed5806d1746cb5dcbae4818
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-06-22 01:45:24 -0700

    timeline available for everyone

--- pkg/slinga/visibility/view_timeline.go
+++ pkg/slinga/visibility/view_timeline.go
@@ -24,11 +24,12 @@ func NewTimelineView(userID string, states map[int]slinga.ServiceUsageState, use
 
 // GetData returns data for a given view
 func (view TimelineView) GetData() interface{} {
-	// table only exists for global ops people
 	result := lineEntryList{}
+	/*
 	if !view.users.Users[view.userID].IsGlobalOps() {
 		return result
 	}
+	*/
 	for revisionNumber, state := range view.states {
 		rev := slinga.AptomiRevision(revisionNumber)
 		entry := lineEntry{
