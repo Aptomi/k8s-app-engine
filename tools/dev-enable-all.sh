8e1426eee864cfc88879016349a8de672e9bfd1d
Sergey Lukjanov <me@slukjanov.name>
2017-06-22 18:04:07 -0700

    Improve enable all script

--- tools/dev-enable-all.sh
+++ tools/dev-enable-all.sh
@@ -4,4 +4,5 @@ set -eou pipefail
 
 echo "APTOMI_DB=$APTOMI_DB"
 
+grep -rl '^enabled: false' $APTOMI_DB/policy/ | xargs sed -i '' 's/^enabled: false/enabled: true/g'
 grep -rl '^  enabled: false' $APTOMI_DB/policy/ | xargs sed -i '' 's/^  enabled: false/  enabled: true/g'
