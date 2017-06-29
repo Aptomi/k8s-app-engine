3cf6244df768d8f10fac47452134805699896d65
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-06-28 18:43:57 -0700

    fixed auth to work with LDAP

--- pkg/slinga/server/auth.go
+++ pkg/slinga/server/auth.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/Frostman/aptomi/pkg/slinga/language"
 	"net/http"
 	"time"
+	"fmt"
 )
 
 func getLoggedInUserID(r *http.Request) string {
@@ -15,7 +16,15 @@ func getLoggedInUserID(r *http.Request) string {
 }
 
 func isUnauthorized(r *http.Request) bool {
-	return len(getLoggedInUserID(r)) <= 0
+	userID := getLoggedInUserID(r)
+	if len(userID) <= 0 {
+		return true
+	}
+	user := NewAptomiUserLoader().LoadUserByID(userID)
+	if user == nil {
+		return true
+	}
+	return false
 }
 
 func requireAuth(handler http.HandlerFunc) http.HandlerFunc {
@@ -31,12 +40,20 @@ func requireAuth(handler http.HandlerFunc) http.HandlerFunc {
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	userID := r.URL.Query().Get("logUserID")
+	userID = handleShortcutDemoIDs(userID)
 	user := NewAptomiUserLoader().LoadUserByID(userID)
 
-	http.SetCookie(w, &http.Cookie{Name: "logUserID", Value: userID, Path: "/"})
-	http.SetCookie(w, &http.Cookie{Name: "logUserName", Value: user.Name, Path: "/"})
-	http.SetCookie(w, &http.Cookie{Name: "logUserDescr", Value: user.Labels["short-description"], Path: "/"})
+	if user != nil {
+		http.SetCookie(w, &http.Cookie{Name: "logUserID", Value: userID, Path: "/"})
+		http.SetCookie(w, &http.Cookie{Name: "logUserName", Value: user.Name, Path: "/"})
+		http.SetCookie(w, &http.Cookie{Name: "logUserDescr", Value: user.Labels["short-description"], Path: "/"})
+	}
 	handleAutoRedirect(w, r)
+}
+
+func handleShortcutDemoIDs(userID string) string {
+	// TODO: remove authentication shortcut
+	return fmt.Sprintf("cn=%s,ou=people,o=aptomiOrg", userID)
 }
 
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
