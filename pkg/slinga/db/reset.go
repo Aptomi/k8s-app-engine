7284ea43b938e3efd8cb53879a25d95e887e9f27
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-06-28 17:32:18 -0700

    because of incorrect gitignore, contents of "db" package were never committed to git. fixing this!

---
+++ pkg/slinga/db/reset.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"fmt"
+	. "github.com/Frostman/aptomi/pkg/slinga/log"
+	. "github.com/Frostman/aptomi/pkg/slinga/util"
+	log "github.com/Sirupsen/logrus"
+)
+
+// ResetAptomiState fully resets aptomi state by deleting all files and directories from its database
+// That includes all revisions of policy, resolution data, logs, etc
+func ResetAptomiState() {
+	baseDir := GetAptomiBaseDir()
+	Debug.WithFields(log.Fields{
+		"baseDir": baseDir,
+	}).Info("Resetting aptomi state")
+
+	err := DeleteDirectoryContents(baseDir)
+	if err != nil {
+		Debug.WithFields(log.Fields{
+			"directory": baseDir,
+			"error":     err,
+		}).Panic("Directory contents can't be deleted")
+	}
+
+	fmt.Println("Aptomi state is now empty. Deleted all objects")
+}
---
+++ pkg/slinga/db/revision_test.go
@@ -0,0 +1,14 @@
+package db
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRevision(t *testing.T) {
+	revision := GetLastRevision("../testdata/unittests")
+	assert.Equal(t, AptomiRevision(239), revision, "Correct revision expected")
+
+	revisionNonExistent := GetLastRevision("../testdata/unittests/non-existent")
+	assert.Equal(t, AptomiRevision(LastRevisionAbsentValue), revisionNonExistent, "Correct initial revision expected")
+}
