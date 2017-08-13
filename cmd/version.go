d01085dcf1bd474c62f9cc364e0144214d88555e
Sergey Lukjanov <me@slukjanov.name>
2017-08-12 20:20:01 -0700

    Fix short description for version command

--- cmd/version.go
+++ cmd/version.go
@@ -15,7 +15,7 @@ var BuildTime string
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version string of Aptomi",
+	Short: "Print the Aptomi version",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("Aptomi version: %s\n       git commit: %s\n       built: %s\n", Version, GitCommit, BuildTime)
 	},
