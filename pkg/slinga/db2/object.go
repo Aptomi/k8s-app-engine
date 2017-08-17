8c176bad5cef81ff9b64d76fd11eebaf2540ba5a
Sergey Lukjanov <me@slukjanov.name>
2017-08-17 15:25:16 -0700

    Add Object interface and BaseObject impl

---
+++ pkg/slinga/db2/object.go
@@ -0,0 +1,68 @@
+package db2
+
+// ObjectKind represents object name like Cluster, Service, Context, etc.
+type ObjectKind string
+
+// Object interface represents unified object that could be stored in DB, accessed through API, etc.
+type Object interface {
+	GetKind() ObjectKind
+	GetKey() Key
+	GetUID() UID
+	GetGeneration() Generation
+	GetName() string
+	GetNamespace() string
+	GetSpec() interface{}
+
+	// TODO(slukjanov): should any object have owner?
+}
+
+// BaseObject provides basic implementation of the Object interface
+type BaseObject struct {
+	Kind     ObjectKind
+	Metadata BaseObjectMetadata
+	Spec     interface{}
+}
+
+type BaseObjectMetadata struct {
+	UID        UID
+	Generation Generation
+	Name       string
+	Namespace  string
+	// TODO(slukjanov): do we need CreatedAt string? I think yes
+}
+
+// GetKind returns object's ObjectKind
+func (object *BaseObject) GetKind() ObjectKind {
+	return object.Kind
+}
+
+// GetUID returns object's UID
+func (object *BaseObject) GetUID() UID {
+	return object.Metadata.UID
+}
+
+// GetGeneration returns object's Generation ("version")
+func (object *BaseObject) GetGeneration() Generation {
+	return object.Metadata.Generation
+}
+
+// TODO(slukjanov): should we only store key or uid / gen? or cache key inside metadata?
+// GetKey returns object's Key
+func (object *BaseObject) GetKey() Key {
+	return KeyFromParts(object.Metadata.UID, object.Metadata.Generation)
+}
+
+// GetNamespace returns object's Namespace
+func (object *BaseObject) GetNamespace() string {
+	return object.Metadata.Namespace
+}
+
+// GetName returns object's Name
+func (object *BaseObject) GetName() string {
+	return object.Metadata.Name
+}
+
+// GetSpec returns object's Spec
+func (object *BaseObject) GetSpec() interface{} {
+	return object.Spec
+}
---
+++ pkg/slinga/db2/object_test.go
@@ -0,0 +1,29 @@
+package db2
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBaseObject(t *testing.T) {
+	var obj Object = &BaseObject{
+		"kind",
+		BaseObjectMetadata{
+			"uid",
+			42,
+			"name",
+			"namespace",
+		},
+		map[string]string{
+			"a": "b",
+		},
+	}
+
+	assert.Equal(t, ObjectKind("kind"), obj.GetKind(), "Correct ObjectKind expected")
+	assert.Equal(t, UID("uid"), obj.GetUID(), "Correct UID expected")
+	assert.Equal(t, Generation(42), obj.GetGeneration(), "Correct Generation expected")
+	assert.Equal(t, KeyFromParts("uid", 42), obj.GetKey(), "Correct Key expected")
+	assert.Equal(t, "namespace", obj.GetNamespace(), "Correct Namespace expected")
+	assert.Equal(t, "name", obj.GetName(), "Correct Name expected")
+	assert.NotNil(t, obj.GetSpec(), "NotNil Spec expected")
+}
