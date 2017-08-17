c1b106bad8b769a7e8b2098fb0e24899820dbd50
Sergey Lukjanov <me@slukjanov.name>
2017-08-17 14:22:40 -0700

    Move uuid to db2/base and add comments

--- pkg/slinga/db2/base.go
+++ pkg/slinga/db2/base.go
@@ -1,15 +1,53 @@
 package db2
 
 import (
-	"github.com/Aptomi/aptomi/pkg/slinga/util"
+	"fmt"
+	"github.com/pborman/uuid"
 	"strconv"
 	"strings"
+	"sync"
 )
 
+// Generation represents object's "version" and starts from 0
 type Generation uint64
 
+// UID represents unique object ID.
+// It's needed because object with some name could be removed and than created again and in this case generation should
+// start from 0 again. In this case, on new object creation new UID always created that guarantees differentiation
+// between objects created with same name and deleted.
+type UID string
+
+// It helps to make sure that UID is unique
+var (
+	uuidCreationLock sync.Mutex
+	lastCreatedUUID  uuid.UUID
+)
+
+// NewUUID creates new guaranteed unique thread-safe unique ID
+func NewUUID() UID {
+	uuidCreationLock.Lock()
+	defer uuidCreationLock.Unlock()
+
+	newUUID := uuid.NewUUID()
+
+	// Identical UUIDs could be generated in case of small time interval
+	// between NewUUID calls. Let's wait until new UUID generated.
+	// UUID uses 100 ns increments, so, it's okay to just poll for new value
+	for uuid.Equal(lastCreatedUUID, newUUID) == true {
+		fmt.Println("Same UUID generated!!!")
+		newUUID = uuid.NewUUID()
+	}
+	lastCreatedUUID = newUUID
+
+	return UID(newUUID.String())
+}
+
+// KeySeparator used to separate UID and Generation inside the Key
 const KeySeparator = "$"
 
+// Key represents unified object's key that includes object's UID and generation.
+// So, it means that Key could be used to reference concrete object with concrete
+// generation (while UID is a reference to the concrete object but any generation).
 type Key string
 
 func (key Key) parts() []string {
@@ -20,10 +58,12 @@ func (key Key) parts() []string {
 	return parts
 }
 
-func (key Key) GetUID() util.UID {
-	return util.UID(key.parts()[0])
+// GetUID returns UID part of the Key
+func (key Key) GetUID() UID {
+	return UID(key.parts()[0])
 }
 
+// GetGeneration returns Generation part of the Key
 func (key Key) GetGeneration() Generation {
 	val, err := strconv.ParseUint(key.parts()[1], 10, 64)
 	if err != nil {
--- pkg/slinga/db2/base_test.go
+++ pkg/slinga/db2/base_test.go
@@ -1,7 +1,6 @@
 package db2
 
 import (
-	"github.com/Aptomi/aptomi/pkg/slinga/util"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -9,7 +8,7 @@ import (
 func TestKey(t *testing.T) {
 	correctKey := Key("72b062c1-7fcf-11e7-ab09-acde48001122$42")
 
-	assert.Equal(t, util.UID("72b062c1-7fcf-11e7-ab09-acde48001122"), correctKey.GetUID(), "Correct UID expected")
+	assert.Equal(t, UID("72b062c1-7fcf-11e7-ab09-acde48001122"), correctKey.GetUID(), "Correct UID expected")
 	assert.Equal(t, Generation(42), correctKey.GetGeneration(), "Correct Generation expected")
 
 	noGenerationKey := Key("72b062c1-7fcf-11e7-ab09-acde48001122")
@@ -19,6 +18,6 @@ func TestKey(t *testing.T) {
 
 	invalidGenerationKey := Key("72b062c1-7fcf-11e7-ab09-acde48001122$bad")
 
-	assert.Equal(t, util.UID("72b062c1-7fcf-11e7-ab09-acde48001122"), correctKey.GetUID(), "Correct UID expected")
+	assert.Equal(t, UID("72b062c1-7fcf-11e7-ab09-acde48001122"), correctKey.GetUID(), "Correct UID expected")
 	assert.Panics(t, func() { invalidGenerationKey.GetGeneration() }, "Panic expected if key is incorrect")
 }
