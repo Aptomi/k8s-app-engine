068f5d73a1468af140a5341fc3df866d1d946477
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-08-19 01:01:42 -0700

    100% unit test coverage for labels and label ops

--- pkg/slinga/language/labelset_test.go
+++ pkg/slinga/language/labelset_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestLabelOperations(t *testing.T) {
+func TestLabelSetOperations(t *testing.T) {
 	labelsBefore := NewLabelSet(map[string]string{"l1": "1", "l2": "2", "l3": "3"})
 
 	ops := NewLabelOperations(
@@ -25,3 +25,46 @@ func TestLabelOperations(t *testing.T) {
 	labelsAfter = labelsAfter.ApplyTransform(nil)
 	assert.Equal(t, 4, len(labelsAfter.Labels), "Correct number of labels should be retained after transform")
 }
+
+func TestLabelSetOperationsSingleLabel(t *testing.T) {
+	labelsBefore := NewLabelSet(map[string]string{"l1": "1", "l2": "2", "l3": "3"})
+	ops := NewLabelOperationsSetSingleLabel("name", "value")
+
+	labelsAfter := labelsBefore.ApplyTransform(ops)
+	labelsEqual := NewLabelSet(map[string]string{"l1": "1", "l2": "2", "l3": "3", "name": "value"})
+	assert.True(t, labelsAfter.Equal(labelsEqual), "Label sets ops when setting a single label should work")
+}
+
+func TestLabelSetEquals(t *testing.T) {
+	labelsBefore := NewLabelSet(map[string]string{"a": "1", "b": "2", "c": "3"})
+
+	ops := NewLabelOperations(
+		map[string]string{"d": "4", "e": "5", "a": "aValue"},
+		map[string]string{"c": ""},
+	)
+
+	labelsAfter := labelsBefore.ApplyTransform(ops)
+
+	// check for equal
+	labelsEqual := NewLabelSet(map[string]string{"a": "aValue", "b": "2", "d": "4", "e": "5"})
+	assert.True(t, labelsAfter.Equal(labelsEqual), "Label sets should be equal")
+
+	// check for not equal
+	labelsNotEqual := NewLabelSet(map[string]string{"b": "2", "d": "someValue", "e": "5"})
+	assert.False(t, labelsAfter.Equal(labelsNotEqual), "Label sets should not be equal")
+
+	// check for equal (map with zero lengths and nil)
+	labelsEmpty := NewLabelSet(map[string]string{})
+	assert.True(t, labelsEmpty.Equal(NewLabelSet(nil)), "Empty label sets should be equal")
+}
+
+func TestLabelSetAdd(t *testing.T) {
+	labels1 := NewLabelSet(map[string]string{"a": "1", "b": "2", "c": "3"})
+	labels2 := NewLabelSet(map[string]string{"c": "4", "d": "5", "e": "6"})
+	labelsAfter := labels1.AddLabels(labels2)
+
+	// check for equal
+	labelsEqual := NewLabelSet(map[string]string{"a": "1", "b": "2", "c": "4", "d": "5", "e": "6"})
+	assert.True(t, labelsAfter.Equal(labelsEqual), "Label sets addition should work")
+}
+
