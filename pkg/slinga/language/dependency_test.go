fdcd151050c43e928bea5d4ed80f2c137f44872d
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-08-19 00:50:12 -0700

    added unit tests for dependencies

--- pkg/slinga/language/dependency_test.go
+++ pkg/slinga/language/dependency_test.go
@@ -7,10 +7,44 @@ import (
 
 func TestLoadDependencies(t *testing.T) {
 	dependencies := loadUnitTestsPolicy().Dependencies
-	assert.Equal(t, 4, len(dependencies.DependenciesByService["kafka"]), "Correct number of dependencies should be loaded")
-	assert.Equal(t, "dep_id_1", dependencies.DependenciesByService["kafka"][0].GetID(), "Dependency ID should be correct")
-	assert.Equal(t, 0, len(dependencies.DependenciesByService["kafka"][0].Labels), "First dependency should have 0 labels")
-	assert.Equal(t, 2, len(dependencies.DependenciesByService["kafka"][1].Labels), "Second dependency should have 1 label")
-	assert.Equal(t, 0, len(dependencies.DependenciesByService["kafka"][2].Labels), "Third dependency should have 0 labels")
-	assert.Equal(t, 0, len(dependencies.DependenciesByService["kafka"][3].Labels), "Fourth dependency should have 0 labels")
+
+	// look at kafka dependencies
+	kafkaDeps := dependencies.DependenciesByService["kafka"]
+
+	// check dependency map
+	assert.Equal(t, 4, len(kafkaDeps), "Correct number of dependencies should be loaded")
+	assert.Equal(t, "dep_id_1", kafkaDeps[0].GetID(), "Dependency ID should be correct")
+	assert.Equal(t, 0, len(kafkaDeps[0].Labels), "First dependency should have 0 labels")
+	assert.Equal(t, 2, len(kafkaDeps[1].Labels), "Second dependency should have 1 label")
+	assert.Equal(t, 0, len(kafkaDeps[2].Labels), "Third dependency should have 0 labels")
+	assert.Equal(t, 0, len(kafkaDeps[3].Labels), "Fourth dependency should have 0 labels")
+
+	// check dependency labels through a label set
+	dep2 := dependencies.DependenciesByID["dep_id_2"]
+	assert.Equal(t, 2, len(dep2.GetLabelSet().Labels), "dep_id_2's labelset should have correct length")
+	assert.Equal(t, "yes", dep2.GetLabelSet().Labels["important"], "dep_id_2's should have important='yes' label through a labelset")
+	assert.Equal(t, "yes", dep2.GetLabelSet().Labels["some-label-to-be-removed"], "dep_id_2's should have some-label-to-be-removed='yes' label through a labelset")
+}
+
+func TestAddDependency(t *testing.T) {
+	dependencies := loadUnitTestsPolicy().Dependencies
+
+	// create new dependency
+	depAdd := &Dependency{
+		SlingaObject: &SlingaObject{
+			Metadata: SlingaObjectMetadata{
+				Namespace: "main",
+				Name:      "dep_id_new",
+			}},
+		UserID:  "612",
+		Service: "newservice",
+	}
+
+	// add it to the list of global dependencies
+	dependencies.AddDependency(depAdd)
+
+	// check that it was successfully added
+	assert.Equal(t, 1, len(dependencies.DependenciesByService["newservice"]), "Dependency on 'newservice' should be added")
+	assert.Equal(t, "dep_id_new", dependencies.DependenciesByService["newservice"][0].GetName(), "Dependency on 'newservice' should be added")
+	assert.Equal(t, "dep_id_new", dependencies.DependenciesByID["dep_id_new"].GetName(), "Dependency on 'newservice' should be added")
 }
