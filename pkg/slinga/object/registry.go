61b17746bd7ae22267849c55c1cf791ce80a6771
Sergey Lukjanov <me@slukjanov.name>
2017-08-19 02:36:29 -0700

    Rework registry for better flexibility

--- pkg/slinga/object/registry.go
+++ pkg/slinga/object/registry.go
@@ -1,9 +1,8 @@
 package object
 
 import (
-	. "github.com/Aptomi/aptomi/pkg/slinga/log"
+	"fmt"
 	"github.com/Aptomi/aptomi/pkg/slinga/object/codec"
-	log "github.com/Sirupsen/logrus"
 )
 
 type Constructor func() BaseObject
@@ -13,43 +12,105 @@ type Registry struct {
 	kinds map[string]Constructor
 }
 
-func (registry Registry) AddKind(kind string, specUnmarshaler Constructor) {
-	registry.kinds[kind] = specUnmarshaler
+func (reg *Registry) AddKind(kind string, constructor Constructor) {
+	// todo support default kind?
+	reg.kinds[kind] = constructor
 }
 
-func (registry Registry) MarshalOne(object BaseObject) []byte {
-	data, err := registry.codec.Marshal(&object)
-	if err != nil {
-		Debug.WithFields(log.Fields{
-			"meta":  object,
-			"error": err,
-		}).Panic("Can't marshal meta", err)
-	}
-	return data
+func (reg *Registry) MarshalOne(object BaseObject) ([]byte, error) {
+	return reg.codec.Marshal(&object)
 }
 
-func (registry Registry) UnmarshalOne(data []byte) BaseObject {
-	meta := &emptyObject{}
-	err := registry.codec.Unmarshal(data, meta)
+func (reg *Registry) MarshalMany(objects []BaseObject) ([]byte, error) {
+	return reg.codec.Marshal(&objects)
+}
+
+func (reg *Registry) UnmarshalOne(data []byte) (BaseObject, error) {
+	objects, err := reg.UnmarshalMany(data)
 	if err != nil {
-		Debug.WithFields(log.Fields{
-			"data":  data,
-			"error": err,
-		}).Panic("Can't unmarshal object metadata (empty object)", err)
+		return nil, err
 	}
 
-	kind := meta.Metadata.Kind.String()
-	kindConstructor, ok := registry.kinds[kind]
+	objectsLen := len(objects)
+	if objectsLen != 1 {
+		return nil, fmt.Errorf("Single object expected, but %d unmarshaled", objectsLen)
+	}
+
+	return objects[0], nil
+}
+
+func (reg *Registry) UnmarshalMany(data []byte) ([]BaseObject, error) {
+	raw := new(interface{})
+	err := reg.codec.Unmarshal(data, raw)
+	if err != nil {
+		return nil, fmt.Errorf("Error while unmarshaling data to raw interface{}: %s", err)
+	}
+
+	result := make([]BaseObject, 0)
+
+	if elem, ok := (*raw).(map[interface{}]interface{}); ok { // if it's a single object (map)
+		obj, err := reg.unmarshalRaw(elem, data)
+		if err != nil {
+			return nil, fmt.Errorf("Error while unmarshaling single object: %s", err)
+		}
+
+		result = append(result, obj)
+	} else if slice, ok := (*raw).([]interface{}); ok { // if it's an object slice
+		for idx, rawElem := range slice {
+			elem, ok := rawElem.(map[interface{}]interface{}) // each slice elem should be map
+			if !ok {
+				return nil, fmt.Errorf("Element #%d isn't an object", idx)
+			}
+
+			elemData, err := reg.codec.Marshal(elem) // get []byte for current elem only
+			if err != nil {
+				return nil, fmt.Errorf("Error while unmarshaling element #%d (marshal step): %s", idx, err)
+			}
+
+			obj, err := reg.unmarshalRaw(elem, elemData) // unmarshal to kind type
+			if err != nil {
+				return nil, fmt.Errorf("Error while unmarshaling element #%d (final step): %s", idx, err)
+			}
+
+			result = append(result, obj)
+		}
+	}
+
+	return result, nil
+}
+
+func (reg *Registry) unmarshalRaw(single map[interface{}]interface{}, data []byte) (BaseObject, error) {
+	metaField, ok := single["metadata"]
 	if !ok {
-		Debug.WithFields(log.Fields{
-			"data":  data,
-			"kind":  kind,
-			"error": err,
-		}).Panic("Unknown object kind: %s", kind)
+		return nil, fmt.Errorf("Can't find metadata field inside object: %v", single)
+	}
+
+	meta, ok := metaField.(map[interface{}]interface{})
+	if !ok {
+		return nil, fmt.Errorf("Metadata field isn't a map: %v", single)
+	}
+
+	kindField, ok := meta["kind"]
+	if !ok {
+		return nil, fmt.Errorf("Can't find kind field in metadata: %v", single)
+	}
+
+	kind, ok := kindField.(string)
+	if !ok {
+		return nil, fmt.Errorf("Kind field in metadata isn't a string: %v", single)
+	}
+
+	kindConstructor, ok := reg.kinds[kind]
+	if !ok {
+		// todo support default kind?
+		return nil, fmt.Errorf("Unknown kind: %s", kind)
 	}
 
 	obj := kindConstructor()
-	err = registry.codec.Unmarshal(data, obj)
+	err := reg.codec.Unmarshal(data, obj)
+	if err != nil {
+		return nil, err
+	}
 
-	return obj
+	return obj, nil
 }
--- pkg/slinga/object/registry_test.go
+++ pkg/slinga/object/registry_test.go
@@ -1,43 +1,118 @@
 package object
 
 import (
-	"github.com/Aptomi/aptomi/pkg/slinga/object/codec/fake"
 	"github.com/Aptomi/aptomi/pkg/slinga/object/codec/yaml"
 	"github.com/stretchr/testify/assert"
-	"reflect"
 	"testing"
 )
 
-func TestRegistry(t *testing.T) {
-	reg := Registry{yaml.YamlCodec, make(map[string]Constructor)}
-	reg.AddKind("test", func() BaseObject { return new(testObject) })
+type t1 struct {
+	Metadata
 
-	data := reg.MarshalOne(testObjectInstance)
-	expectedYaml := `metadata:
-  kind: test
-  uid: 4d0e5391-83ef-11e7-876b-784f435b826b
-  generation: 42
-  name: name
-  namespace: namespace
-testfield: test
+	T1 string
+}
+
+type t2 struct {
+	Metadata
+
+	T2 string
+}
+
+func newTestRegistry() *Registry {
+	reg := Registry{yaml.YamlCodec, make(map[string]Constructor)}
+	reg.AddKind("t1", func() BaseObject { return new(t1) })
+	reg.AddKind("t2", func() BaseObject { return new(t2) })
+
+	return &reg
+}
+
+var (
+	testObjs = []BaseObject{
+		&t1{
+			Metadata{
+				Kind: "t1",
+				Name: "t1-name",
+			},
+			"t1p",
+		},
+		&t2{
+			Metadata{
+				Kind: "t2",
+				Name: "t2-name",
+			},
+			"t2p",
+		},
+	}
+	testObjsMarshaled = []string{
+		`metadata:
+  kind: t1
+  uid: ""
+  generation: 0
+  name: t1-name
+  namespace: ""
+t1: t1p
+`,
+		`metadata:
+  kind: t2
+  uid: ""
+  generation: 0
+  name: t2-name
+  namespace: ""
+t2: t2p
+`,
+	}
+	testObjsSliceMarshaled = `- metadata:
+    kind: t1
+    uid: ""
+    generation: 0
+    name: t1-name
+    namespace: ""
+  t1: t1p
+- metadata:
+    kind: t2
+    uid: ""
+    generation: 0
+    name: t2-name
+    namespace: ""
+  t2: t2p
 `
-	assert.Equal(t, expectedYaml, string(data), "Correct marshal yaml string expected")
+)
 
-	newObj := reg.UnmarshalOne(data)
-	assertTestObjectInstance(t, newObj)
-	assert.True(t, reflect.DeepEqual(testObjectInstance, newObj), "Objects should be deep equal")
+func TestRegistry_AddKind(t *testing.T) {
+	reg := newTestRegistry()
+
+	assert.Contains(t, reg.kinds, "t1", "Registry should know kind t1")
+	assert.Contains(t, reg.kinds, "t2", "Registry should know kind t2")
 }
 
-func TestRegistryCodecErrors(t *testing.T) {
-	reg := Registry{fake.ErrCodec, make(map[string]Constructor)}
-	reg.AddKind("test", func() BaseObject { return new(testObject) })
+func TestRegistry_MarshalOne(t *testing.T) {
+	reg := newTestRegistry()
 
-	assert.Panics(t, func() { reg.MarshalOne(testObjectInstance) })
-	assert.Panics(t, func() { reg.UnmarshalOne(make([]byte, 0)) })
+	data, err := reg.MarshalOne(testObjs[0])
+	assert.Nil(t, err, "Object should be marshaled w/o errors")
+	assert.Equal(t, testObjsMarshaled[0], string(data), "Correct marshaled data expected")
 }
 
-func TestRegistryUnknownKind(t *testing.T) {
-	reg := Registry{yaml.YamlCodec, make(map[string]Constructor)}
+func TestRegistry_MarshalMany(t *testing.T) {
+	reg := newTestRegistry()
 
-	assert.Panics(t, func() { reg.UnmarshalOne(make([]byte, 0)) })
+	data, err := reg.MarshalMany(testObjs)
+	assert.Nil(t, err, "Objects should be marshaled w/o errors")
+	assert.Equal(t, testObjsSliceMarshaled, string(data), "Correct marshaled data expected")
+}
+
+func TestRegistry_UnmarshalOne(t *testing.T) {
+	reg := newTestRegistry()
+
+	obj, err := reg.UnmarshalOne([]byte(testObjsMarshaled[0]))
+	assert.Nil(t, err, "Object should be unmarshaled w/o errors")
+	assert.Exactly(t, testObjs[0], obj, "Unmarshaled object should be deep equal to initial one")
+}
+
+func TestRegistry_UnmarshalMany(t *testing.T) {
+	reg := newTestRegistry()
+
+	obj, err := reg.UnmarshalMany([]byte(testObjsSliceMarshaled))
+	assert.Nil(t, err, "Objects should be unmarshaled w/o errors")
+	assert.Exactly(t, testObjs, obj, "Unmarshaled objects should be deep equal to initial ones")
 }
