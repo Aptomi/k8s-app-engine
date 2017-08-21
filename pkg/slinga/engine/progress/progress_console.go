d634d2e5813b10258eae94db5380d6081a167d4e
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-08-20 20:20:44 -0700

    added a simple test to call console indicator (but don't verify its console output)

--- pkg/slinga/engine/progress/progress_console.go
+++ pkg/slinga/engine/progress/progress_console.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"github.com/gosuri/uiprogress"
 	"time"
+	"os"
+	"io"
 )
 
 type ProgressConsole struct {
 	*progressCount
 	progress    *uiprogress.Progress
 	progressBar *uiprogress.Bar
+	out         io.Writer
 }
 
 func NewProgressConsole() *ProgressConsole {
 	progress := uiprogress.New()
+	progress.Out = os.Stdout
 	progress.RefreshInterval = time.Second
 	progress.Start()
 	return &ProgressConsole{progressCount: &progressCount{}, progress: progress}
@@ -21,8 +25,9 @@ func NewProgressConsole() *ProgressConsole {
 
 func (progressConsole *ProgressConsole) createProgressBar() {
 	if progressConsole.getTotalInternal() > 0 {
-		fmt.Println("[Applying changes]")
+		fmt.Println(progressConsole.out, "[Applying changes]")
 	}
+	progressConsole.progress.SetOut(progressConsole.out)
 	progressConsole.progressBar = progressConsole.progress.AddBar(progressConsole.getTotalInternal())
 	progressConsole.progressBar.PrependFunc(func(b *uiprogress.Bar) string {
 		return fmt.Sprintf("  [%s: %d/%d]", progressConsole.getStageInternal(), b.Current(), b.Total)
@@ -31,6 +36,10 @@ func (progressConsole *ProgressConsole) createProgressBar() {
 	progressConsole.progressBar.AppendFunc(func(b *uiprogress.Bar) string {
 		return fmt.Sprintf("  Time: %s", b.TimeElapsedString())
 	})
+}
+
+func (progressConsole *ProgressConsole) SetOut(out io.Writer) {
+	progressConsole.out = out
 }
 
 func (progressConsole *ProgressConsole) SetTotal(total int) {
--- pkg/slinga/engine/progress/progress_noop_test.go
+++ pkg/slinga/engine/progress/progress_noop_test.go
@@ -4,39 +4,48 @@ import (
 	"github.com/stretchr/testify/assert"
 	"math"
 	"testing"
+	"bytes"
 )
 
+func makeProgressIndicators() []ProgressIndicator {
+	p1 := NewProgressNoop()
+	p2 := NewProgressConsole()
+	p2.SetOut(new(bytes.Buffer))
+	return []ProgressIndicator{p1, p2}
+}
+
 func TestProgressNoop(t *testing.T) {
-	progress := NewProgressNoop()
+	for _, progress := range makeProgressIndicators() {
+		// check initial state
+		assert.False(t, progress.IsDone(), "Progress indicator should not be finished yet")
+		assert.Equal(t, 0, progress.GetCompletionPercent(), "Progress indicator should be at 0%")
 
-	// check initial state
-	assert.False(t, progress.IsDone(), "Progress indicator should not be finished yet")
-	assert.Equal(t, 0, progress.GetCompletionPercent(), "Progress indicator should be at 0%")
+		// set total number of steps
+		progress.SetTotal(200)
+		assert.False(t, progress.IsDone(), "Progress indicator should not be finished yet")
 
-	// set total number of steps
-	progress.SetTotal(200)
-	assert.False(t, progress.IsDone(), "Progress indicator should not be finished yet")
+		// advance
+		for i := 0; i < 200; i++ {
+			expectedPercent := int(math.Floor(float64(i) / 2.0))
+			assert.Equal(t, expectedPercent, progress.GetCompletionPercent(), "Progress indicator should be at 25%")
+			progress.Advance("Main")
+		}
 
-	// advance
-	for i := 0; i < 200; i++ {
-		expectedPercent := int(math.Floor(float64(i) / 2.0))
-		assert.Equal(t, expectedPercent, progress.GetCompletionPercent(), "Progress indicator should be at 25%")
-		progress.Advance("Main")
+		// check completion
+		assert.Equal(t, 100, progress.GetCompletionPercent(), "Progress indicator should be at 100%")
+		assert.False(t, progress.IsDone(), "Progress indicator should not be finished yet")
+		progress.Done()
+		assert.True(t, progress.IsDone(), "Progress indicator should be finished")
 	}
-
-	// check completion
-	assert.Equal(t, 100, progress.GetCompletionPercent(), "Progress indicator should be at 100%")
-	assert.False(t, progress.IsDone(), "Progress indicator should not be finished yet")
-	progress.Done()
-	assert.True(t, progress.IsDone(), "Progress indicator should be finished")
 }
 
 func TestProgressNoopOverflow(t *testing.T) {
-	progress := NewProgressNoop()
-	progress.SetTotal(10)
-	for i := 0; i < 200; i++ {
-		progress.Advance("Main")
+	for _, progress := range makeProgressIndicators() {
+		progress.SetTotal(10)
+		for i := 0; i < 200; i++ {
+			progress.Advance("Main")
+		}
+		assert.Equal(t, 100, progress.GetCompletionPercent(), "Progress indicator should be at 100%")
+		assert.False(t, progress.IsDone(), "Progress indicator should not be finished until marked as such")
 	}
-	assert.Equal(t, 100, progress.GetCompletionPercent(), "Progress indicator should be at 100%")
-	assert.False(t, progress.IsDone(), "Progress indicator should not be finished until marked as such")
 }
