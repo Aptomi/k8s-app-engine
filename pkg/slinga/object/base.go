804c34e828ae21f1909aea9b3d72b8d712c05e29
Sergey Lukjanov <me@slukjanov.name>
2017-08-22 21:20:33 -0700

    Switch to github.com/satori/go.uuid for UUID generation

--- pkg/slinga/object/base.go
+++ pkg/slinga/object/base.go
@@ -1,11 +1,9 @@
 package object
 
 import (
-	"fmt"
-	"github.com/pborman/uuid"
+	"github.com/satori/go.uuid"
 	"strconv"
 	"strings"
-	"sync"
 )
 
 // Generation represents object's "version" and starts from 0
@@ -18,7 +16,7 @@ func (generation Generation) String() string {
 
 // UID represents unique object ID.
 // It's needed because object with some name could be removed and than created again and in this case generation should
-// start from 0 again. In this case, on new object creation new UID always created that guarantees differentiation
+// start from 1 again. In this case, on new object creation new UID always created that guarantees differentiation
 // between objects created with same name and deleted.
 type UID string
 
@@ -27,30 +25,9 @@ func (uid UID) String() string {
 	return string(uid)
 }
 
-// It helps to make sure that UID is unique
-var (
-	uuidCreationLock sync.Mutex
-	lastCreatedUUID  uuid.UUID
-)
-
 // NewUUID creates new guaranteed unique thread-safe unique ID
 func NewUUID() UID {
-	uuidCreationLock.Lock()
-	defer uuidCreationLock.Unlock()
-
-	newUUID := uuid.NewUUID()
-
-	// Identical UUIDs could be generated in case of small time interval
-	// between NewUUID calls. Let's wait until new UUID generated.
-	// UUID uses 100 ns increments, so, it's okay to just poll for new value
-	for uuid.Equal(lastCreatedUUID, newUUID) == true {
-		// TODO(slukjanov): replace with some WARN message and/or event
-		fmt.Println("Same UUID generated!!!")
-		newUUID = uuid.NewUUID()
-	}
-	lastCreatedUUID = newUUID
-
-	return UID(newUUID.String())
+	return UID(uuid.NewV1().String())
 }
 
 // KeySeparator used to separate UID and Generation inside the Key
