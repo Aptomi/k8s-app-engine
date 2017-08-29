05d0bb58934f30832f89c0c26ac76103dc402b0c
Sergey Lukjanov <me@slukjanov.name>
2017-08-28 23:32:43 -0700

    Rework test_policy_loader to remove dependency on temp file store

--- pkg/slinga/language/test_policy_loader.go
+++ pkg/slinga/language/test_policy_loader.go
@@ -1,27 +1,22 @@
 package language
 
 import (
+	"fmt"
 	. "github.com/Aptomi/aptomi/pkg/slinga/object"
+	"github.com/Aptomi/aptomi/pkg/slinga/object/codec"
 	"github.com/Aptomi/aptomi/pkg/slinga/object/codec/yaml"
-	. "github.com/Aptomi/aptomi/pkg/slinga/object/store/file"
+	"github.com/mattn/go-zglob"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
 func LoadUnitTestsPolicy() *PolicyNamespace {
-	catalog := NewObjectCatalog()
-	catalog.Add(ServiceObject)
-	catalog.Add(ContextObject)
-	catalog.Add(ClusterObject)
-	catalog.Add(RuleObject)
-	catalog.Add(DependencyObject)
-
-	store := FileStore{}
-	store.Codec = &yaml.YamlCodec{}
-	store.Codec.SetObjectCatalog(catalog)
-	store.Open("../testdata/unittests")
+	loader := NewFileLoader("../testdata/unittests")
 
 	policy := NewPolicyNamespace()
-
-	objects, err := store.LoadObjects()
+	objects, err := loader.LoadObjects()
 	if err != nil {
 		panic("Error while loading test Policy")
 	}
@@ -31,5 +26,47 @@ func LoadUnitTestsPolicy() *PolicyNamespace {
 	}
 
 	return policy
+}
 
+func NewFileLoader(path string) *FileLoader {
+	catalog := NewObjectCatalog(ServiceObject, ContextObject, ClusterObject, RuleObject, DependencyObject)
+
+	return &FileLoader{yaml.NewCodec(catalog), path}
+}
+
+type FileLoader struct {
+	codec codec.MarshalUnmarshaler
+	path  string
+}
+
+func (store *FileLoader) LoadObjects() ([]BaseObject, error) {
+	files, _ := zglob.Glob(filepath.Join(store.path, "**", "*.yaml"))
+	sort.Strings(files)
+
+	result := make([]BaseObject, 0)
+	for _, f := range files {
+		if !strings.Contains(f, "external") {
+			objects, err := store.loadObjectsFromFile(f)
+			if err != nil {
+				return nil, fmt.Errorf("Error while loading objects from file %store: %store", f, err)
+			}
+
+			result = append(result, objects...)
+		}
+	}
+
+	return result, nil
+}
+
+func (store *FileLoader) loadObjectsFromFile(path string) ([]BaseObject, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("Error while reading file %store: %store", path, err)
+	}
+	objects, err := store.codec.UnmarshalOneOrMany(data)
+	if err != nil {
+		return nil, fmt.Errorf("Error while unmarshaling file %store: %store", path, err)
+	}
+
+	return objects, nil
 }
