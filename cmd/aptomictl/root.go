fb514a4c6c4eb051bb92b08c8f769c3fcf438f73
Sergey Lukjanov <me@slukjanov.name>
2017-08-29 18:42:45 -0700

    Fix config file flag and configs processing

--- cmd/aptomictl/root.go
+++ cmd/aptomictl/root.go
@@ -20,43 +20,7 @@ var (
 
 		// parse the configPath if one is provided, or use the defaults
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			if configPath != "" { // if config path provided, use it and don't look for default locations
-				configPath, err := filepath.Abs(configPath)
-				if err != nil {
-					panic(fmt.Sprintf("Error reading filepath: %s", err))
-				}
-
-				if stat, err := os.Stat(configPath); err == nil {
-					if stat.IsDir() { // if dir provided, use only it
-						viper.AddConfigPath(configPath)
-					} else { // if specific file provided, use only it
-						viper.SetConfigFile(configPath)
-					}
-				} else if os.IsNotExist(err) {
-					panic(fmt.Sprintf("Path doesn't exists: %s error: %s", configPath, err))
-				} else {
-					panic(fmt.Sprintf("Error while processing path: %s", err))
-				}
-
-				if err := viper.ReadInConfig(); err != nil {
-					panic(fmt.Sprintf("Failed to read config file: %s with error: %s", configPath, err))
-				}
-			} else { // if no config path provided, search in default places
-				home, err := homedir.Dir()
-				if err != nil {
-					panic(fmt.Sprintf("Can't find homedir: %s", err))
-				}
-
-				// search config in home directory with name ".aptomi/config" (without extension).
-				viper.AddConfigPath(path.Join(home, ".aptomi"))
-				viper.SetConfigName("config")
-			}
-
-			// todo(slukjanov): if no config file found, it's okay, continue with defaults and CLI args
-
-			if err := viper.ReadInConfig(); err != nil {
-				panic(fmt.Sprintf("Can't read configPath: %s", err))
-			}
+			readConfig(configPath)
 		},
 
 		Run: func(cmd *cobra.Command, args []string) {
@@ -68,4 +32,59 @@ var (
 
 func init() {
 	AptomiCtlCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "", "Config file or dir path")
+}
+
+func readConfig(configFilePath string) {
+	if configFilePath != "" { // if config path provided, use it and don't look for default locations
+		configAbsPath, err := filepath.Abs(configFilePath)
+		if err != nil {
+			panic(fmt.Sprintf("Error getting abs path for %s error: %s", configFilePath, err))
+		}
+
+		if stat, err := os.Stat(configAbsPath); err == nil {
+			if stat.IsDir() { // if dir provided, use only it
+				viper.AddConfigPath(configAbsPath)
+			} else { // if specific file provided, use only it
+				viper.SetConfigFile(configAbsPath)
+			}
+		} else if os.IsNotExist(err) {
+			panic(fmt.Sprintf("Path doesn't exists: %s error: %s", configAbsPath, err))
+		} else {
+			panic(fmt.Sprintf("Error while processing path: %s", err))
+		}
+	} else { // if no config path available, search in default places
+		home, err := homedir.Dir()
+		if err != nil {
+			panic(fmt.Sprintf("Can't find homedir: %s", err))
+		}
+
+		defaultConfigDir := path.Join(home, ".aptomi")
+
+		// check all supported config types
+		defaultExists := false
+		for _, supportedType := range viper.SupportedExts {
+			defaultConfigFile := path.Join(defaultConfigDir, "config."+supportedType)
+
+			// if there is no default config file - just skip config parsing
+			if _, err := os.Stat(defaultConfigFile); err == nil {
+				defaultExists = true
+				break
+			}
+		}
+
+		if !defaultExists {
+			// todo(slukjanov): print some log message?
+			return
+		}
+
+		// search config in home directory with name ".aptomi/config" (without extension).
+		viper.AddConfigPath(defaultConfigDir)
+		viper.SetConfigName("config")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(fmt.Sprintf("Can't read config: %s", err))
+	}
+
+	// todo(slukjanov): print provided config from viper
 }
