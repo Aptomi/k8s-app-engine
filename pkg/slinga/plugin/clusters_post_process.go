7db0287b1110ad547c7350ef5e65cd197e0bd455
Sergey Lukjanov <me@slukjanov.name>
2017-09-03 01:35:14 -0700

    Fix formatting in /pkg/slinga/plugin/clusters_post_process.go

--- pkg/slinga/plugin/clusters_post_process.go
+++ pkg/slinga/plugin/clusters_post_process.go
@@ -3,12 +3,12 @@ package plugin
 import (
 	"github.com/Aptomi/aptomi/pkg/slinga/engine/resolve"
 	"github.com/Aptomi/aptomi/pkg/slinga/eventlog"
-	lang "github.com/Aptomi/aptomi/pkg/slinga/language"
 	"github.com/Aptomi/aptomi/pkg/slinga/external"
+	lang "github.com/Aptomi/aptomi/pkg/slinga/language"
 )
 
 type ClustersPostProcessPlugin interface {
 	Plugin
 
-	Process(desiredPolicy *lang.PolicyNamespace, desiredState *resolve.PolicyResolution, externalData  *external.Data, eventLog *eventlog.EventLog) error
+	Process(desiredPolicy *lang.PolicyNamespace, desiredState *resolve.PolicyResolution, externalData *external.Data, eventLog *eventlog.EventLog) error
 }
