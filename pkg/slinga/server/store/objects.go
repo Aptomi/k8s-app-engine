1bb2f5f9d6f2512299c4d9e6052a3ed0ff7015c4
Sergey Lukjanov <me@slukjanov.name>
2017-10-08 22:50:05 -0700

    Add revision data to server store objects

--- pkg/slinga/server/store/objects.go
+++ pkg/slinga/server/store/objects.go
@@ -3,5 +3,5 @@ package store
 import "github.com/Aptomi/aptomi/pkg/slinga/object"
 
 var (
-	Objects = []*object.Info{PolicyDataObject}
+	Objects = []*object.Info{PolicyDataObject, RevisionDataObject}
 )
