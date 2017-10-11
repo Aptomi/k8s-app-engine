525886ddeb667307dd769934d4c9656ea22c5ef1
Sergey Lukjanov <me@slukjanov.name>
2017-10-11 04:20:01 -0700

    Fix dir validator

--- pkg/slinga/config/validate.go
+++ pkg/slinga/config/validate.go
@@ -7,6 +7,11 @@ import (
 
 func init() {
 	govalidator.TagMap["dir"] = govalidator.Validator(func(path string) bool {
+		// if path is an empty string - skip validation, should be enforced by "required" validation
+		if len(path) == 0 {
+			return true
+		}
+
 		if stat, err := os.Stat(path); err == nil && stat.IsDir() {
 			return true
 		}
