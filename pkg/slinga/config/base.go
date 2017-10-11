b2ea6134323532695647f3b8e91f12b44b7ffbd6
Sergey Lukjanov <me@slukjanov.name>
2017-10-11 04:52:26 -0700

    Update config

--- pkg/slinga/config/base.go
+++ pkg/slinga/config/base.go
@@ -1,4 +1,5 @@
 package config
 
 type Base interface {
+	IsDebug() bool
 }
--- pkg/slinga/config/server.go
+++ pkg/slinga/config/server.go
@@ -1,17 +1,40 @@
 package config
 
-import "fmt"
-
 type Server struct {
-	Debug  bool
-	Server server
+	Debug      bool
+	Server     serverServer
+	Helm       Helm
+	LDAP       LDAP
+	SecretsDir string `valid:"dir"`
 }
 
-type server struct {
+func (s Server) IsDebug() bool {
+	return s.Debug
+}
+
+type serverServer struct {
 	Host string `valid:"host,required"`
 	Port string `valid:"port,required"`
 }
 
-func (s *server) ListenAddr() string {
-	return fmt.Sprintf("%s:%s", s.Host, s.Port)
+// LDAP contains configuration for LDAP sync service (host, port, DN, filter query and mapping of LDAP properties to Aptomi attributes)
+type LDAP struct {
+	Host              string
+	Port              int
+	BaseDN            string
+	Filter            string
+	LabelToAttributes map[string]string
+}
+
+// GetAttributes Returns the list of attributes to be retrieved from LDAP
+func (cfg *LDAP) GetAttributes() []string {
+	result := []string{}
+	for _, attr := range cfg.LabelToAttributes {
+		result = append(result, attr)
+	}
+	return result
+}
+
+type Helm struct {
+	ChartsDir string `valid:"dir,required"`
 }
