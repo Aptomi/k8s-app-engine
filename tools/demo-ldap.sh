e08819601bf4d7636619b8b5a350f1d78292d53f
Sergey Lukjanov <me@slukjanov.name>
2017-10-10 22:26:01 -0700

    Improve demo-ldap.sh script
    * Use published docker image aptomi/ldap-demo:latest
    * Don't fail if no container running

--- tools/demo-ldap.sh
+++ tools/demo-ldap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker rm -f aptomi-ldap-demo
-docker run --name aptomi-ldap-demo -d -p 10389:10389 aptomi-ldap-demo:latest
+set -ex
+
+docker rm -f aptomi-ldap-demo || true
+docker run --name aptomi-ldap-demo -d -p 10389:10389 aptomi/ldap-demo:latest
