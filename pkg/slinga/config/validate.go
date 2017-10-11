2e526d26250ea75e28e9843da55d1dfc010b7fa7
Sergey Lukjanov <me@slukjanov.name>
2017-10-11 03:13:20 -0700

    Add dir validator

--- pkg/slinga/config/validate.go
+++ pkg/slinga/config/validate.go
@@ -2,7 +2,17 @@ package config
 
 import (
 	"github.com/asaskevich/govalidator"
+	"os"
 )
+
+func init() {
+	govalidator.TagMap["dir"] = govalidator.Validator(func(path string) bool {
+		if stat, err := os.Stat(path); err == nil && stat.IsDir() {
+			return true
+		}
+		return false
+	})
+}
 
 func Validate(config Base) (bool, error) {
 	return govalidator.ValidateStruct(config)
