71b6936e307b1c004b725daf2bbb89ee69f05f34
Sergey Lukjanov <me@slukjanov.name>
2017-10-11 19:59:27 -0700

    Fix config validator test

--- pkg/config/validate_test.go
+++ pkg/config/validate_test.go
@@ -14,72 +14,72 @@ func TestValidate(t *testing.T) {
 		{
 			"success-0.0.0.0:80",
 			&Server{
-				Server: serverServer{Host: "0.0.0.0", Port: "80"},
-				Helm:   Helm{ChartsDir: "/tmp"},
+				API:  API{Host: "0.0.0.0", Port: "80"},
+				Helm: Helm{ChartsDir: "/tmp"},
 			},
 			true,
 		},
 		{
 			"success-0.0.0.0:80",
 			&Server{
-				Server: serverServer{Host: "0.0.0.0", Port: "80"},
-				Helm:   Helm{ChartsDir: ""},
+				API:  API{Host: "0.0.0.0", Port: "80"},
+				Helm: Helm{ChartsDir: ""},
 			},
 			false,
 		},
 		{
 			"success-0.0.0.0:80",
 			&Server{
-				Server: serverServer{Host: "0.0.0.0", Port: "80"},
-				Helm:   Helm{ChartsDir: "/nonexistingdirectoryinroot"},
+				API:  API{Host: "0.0.0.0", Port: "80"},
+				Helm: Helm{ChartsDir: "/nonexistingdirectoryinroot"},
 			},
 			false,
 		},
 		{
 			"success-127.0.0.1:8080",
 			&Server{
-				Server: serverServer{Host: "127.0.0.1", Port: "8080"},
-				Helm:   Helm{ChartsDir: "/tmp"},
+				API:  API{Host: "127.0.0.1", Port: "8080"},
+				Helm: Helm{ChartsDir: "/tmp"},
 			},
 			true,
 		},
 		{
 			"success-10.20.30.40:65080",
 			&Server{
-				Server: serverServer{Host: "10.20.30.40", Port: "65080"},
-				Helm:   Helm{ChartsDir: "/tmp"},
+				API:  API{Host: "10.20.30.40", Port: "65080"},
+				Helm: Helm{ChartsDir: "/tmp"},
 			},
 			true,
 		},
 		{
 			"success-demo.aptomi.io:65080",
 			&Server{
-				Server: serverServer{Host: "demo.aptomi.io", Port: "65080"},
-				Helm:   Helm{ChartsDir: "/tmp"},
+				API:  API{Host: "demo.aptomi.io", Port: "65080"},
+				Helm: Helm{ChartsDir: "/tmp"},
 			},
 			true,
 		},
 		{
 			"fail-0.0.0.0:0",
 			&Server{
-				Server: serverServer{Host: "0.0.0.0", Port: "0"},
-				Helm:   Helm{ChartsDir: "/tmp"},
+				API:  API{Host: "0.0.0.0", Port: "0"},
+				Helm: Helm{ChartsDir: "/tmp"},
 			},
 			false,
 		},
 		{
 			"fail-0.0.0.0:-1",
 			&Server{
-				Server: serverServer{Host: "0.0.0.0", Port: "-1"},
-				Helm:   Helm{ChartsDir: "/tmp"},
+				API:  API{Host: "0.0.0.0", Port: "-1"},
+				Helm: Helm{ChartsDir: "/tmp"},
 			},
 			false,
 		},
 		{
 			"fail-:80",
 			&Server{
-				Server: serverServer{Host: "", Port: "80"},
-				Helm:   Helm{ChartsDir: "/tmp"},
+				API:  API{Host: "", Port: "80"},
+				Helm: Helm{ChartsDir: "/tmp"},
 			},
 			false,
 		},
