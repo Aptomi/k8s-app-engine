1cfbf3d89cdb24190a95cf271d1ed5bfa8369cbc
Sergey Lukjanov <me@slukjanov.name>
2017-10-11 20:36:10 -0700

    Use separated test struct for config validator testing

--- pkg/config/validate_test.go
+++ pkg/config/validate_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type testStruct struct {
+	Host      string `valid:"host,required"`
+	Port      string `valid:"port,required"`
+	ChartsDir string `valid:"dir,required"`
+}
+
+func (t *testStruct) IsDebug() bool {
+	return false
+}
+
 func TestValidate(t *testing.T) {
 	tests := []struct {
 		name   string
@@ -13,73 +23,82 @@ func TestValidate(t *testing.T) {
 	}{
 		{
 			"success-0.0.0.0:80",
-			&Server{
-				API:  API{Host: "0.0.0.0", Port: "80"},
-				Helm: Helm{ChartsDir: "/tmp"},
+			&testStruct{
+				Host:      "0.0.0.0",
+				Port:      "80",
+				ChartsDir: "/tmp",
 			},
 			true,
 		},
 		{
 			"success-0.0.0.0:80",
-			&Server{
-				API:  API{Host: "0.0.0.0", Port: "80"},
-				Helm: Helm{ChartsDir: ""},
+			&testStruct{
+				Host:      "0.0.0.0",
+				Port:      "80",
+				ChartsDir: "",
 			},
 			false,
 		},
 		{
 			"success-0.0.0.0:80",
-			&Server{
-				API:  API{Host: "0.0.0.0", Port: "80"},
-				Helm: Helm{ChartsDir: "/nonexistingdirectoryinroot"},
+			&testStruct{
+				Host:      "0.0.0.0",
+				Port:      "80",
+				ChartsDir: "/nonexistingdirectoryinroot",
 			},
 			false,
 		},
 		{
 			"success-127.0.0.1:8080",
-			&Server{
-				API:  API{Host: "127.0.0.1", Port: "8080"},
-				Helm: Helm{ChartsDir: "/tmp"},
+			&testStruct{
+				Host:      "127.0.0.1",
+				Port:      "8080",
+				ChartsDir: "/tmp",
 			},
 			true,
 		},
 		{
 			"success-10.20.30.40:65080",
-			&Server{
-				API:  API{Host: "10.20.30.40", Port: "65080"},
-				Helm: Helm{ChartsDir: "/tmp"},
+			&testStruct{
+				Host:      "10.20.30.40",
+				Port:      "65080",
+				ChartsDir: "/tmp",
 			},
 			true,
 		},
 		{
 			"success-demo.aptomi.io:65080",
-			&Server{
-				API:  API{Host: "demo.aptomi.io", Port: "65080"},
-				Helm: Helm{ChartsDir: "/tmp"},
+			&testStruct{
+				Host:      "demo.aptomi.io",
+				Port:      "65080",
+				ChartsDir: "/tmp",
 			},
 			true,
 		},
 		{
 			"fail-0.0.0.0:0",
-			&Server{
-				API:  API{Host: "0.0.0.0", Port: "0"},
-				Helm: Helm{ChartsDir: "/tmp"},
+			&testStruct{
+				Host:      "0.0.0.0",
+				Port:      "0",
+				ChartsDir: "/tmp",
 			},
 			false,
 		},
 		{
 			"fail-0.0.0.0:-1",
-			&Server{
-				API:  API{Host: "0.0.0.0", Port: "-1"},
-				Helm: Helm{ChartsDir: "/tmp"},
+			&testStruct{
+				Host:      "0.0.0.0",
+				Port:      "-1",
+				ChartsDir: "/tmp",
 			},
 			false,
 		},
 		{
 			"fail-:80",
-			&Server{
-				API:  API{Host: "", Port: "80"},
-				Helm: Helm{ChartsDir: "/tmp"},
+			&testStruct{
+				Host:      "",
+				Port:      "80",
+				ChartsDir: "/tmp",
 			},
 			false,
 		},
