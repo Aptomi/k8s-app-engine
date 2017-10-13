59603661062299e57d78ee1547ab5b6ef324ea24
Sergey Lukjanov <me@slukjanov.name>
2017-10-13 16:35:47 -0700

    Move API handler to report version from version to api package

---
+++ pkg/server/api/version.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"github.com/Aptomi/aptomi/pkg/server/api/reqresp"
+	"github.com/Aptomi/aptomi/pkg/version"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func handleVersion(r *http.Request, p httprouter.Params) reqresp.Response {
+	return version.GetBuildInfo()
+}
