edcb323f7e19aeba6f546d42ac79654d4c04ccdf
Sergey Lukjanov <me@slukjanov.name>
2017-10-13 17:05:17 -0700

    Add first version of smoke script

---
+++ tools/smoke.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -eou pipefail
+
+# generate temp folder for aptomi config and db
+# make trap that will cleanup temp directory
+# generate smoke config file with disabled apply
+# run server in background with forwarded logging into file server.log + capture pid
+# make trap that will stop server and print all logs to console
+# run cli commands to put policy
+# make trap that will print all client logs to console
+# run some validation ? validate number and list of actions
+
+CONF_DIR=$(mktemp -d)
+
+function cleanup() {
+    kill ${SERVER_PID} || true
+    # todo run if file exists else write warning messege + add header/footer like === being of .... / end of ... ===
+    [[ -e "${CONF_DIR}/server.log" ]] && awk '{print "[[SERVER]] " $0}' ${CONF_DIR}/server.log || echo "No server log found."
+    [[ -e "${CONF_DIR}/client.log" ]] && awk '{print "[[CLIENT]] " $0}' ${CONF_DIR}/client.log || echo "No client log found."
+    rm -rf ${CONF_DIR}
+}
+
+trap cleanup EXIT
+
+function free_port() {
+    for port in $(seq 10000 11000); do
+        echo -ne "\035" | telnet 127.0.0.1 $port > /dev/null 2>&1;
+        [ $? -eq 1 ] && echo "$port" && break;
+    done
+}
+
+APTOMI_PORT=$(free_port)
+
+cat >${CONF_DIR}/config.yaml <<EOL
+debug: true
+
+api:
+  host: 127.0.0.1
+  port: ${APTOMI_PORT}
+
+db:
+  connection: ${CONF_DIR}/db.bolt
+
+ldap:
+  host: localhost
+  port: 10389
+  basedn: o=aptomiOrg
+  filter: (&(objectClass=organizationalPerson))
+  labeltoattributes:
+    id: dn
+    name: cn
+    description: description
+    global_ops: isglobalops
+    is_operator: isoperator
+    mail: mail
+    team: team
+    org: o
+    short-description: role
+    deactivated: deactivated
+EOL
+
+aptomi server --config ${CONF_DIR} &>${CONF_DIR}/server.log &
+SERVER_PID=$!
+
+echo "Server PID: ${SERVER_PID}"
+
+sleep 3
+
+aptomictl policy apply --config ${CONF_DIR} -f demo/policy &>${CONF_DIR}/client.log
