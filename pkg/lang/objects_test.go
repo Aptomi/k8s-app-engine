9c3e2ab72c2651e24f8ef9064fd82e6d6807cafc
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-10-19 17:51:44 -0700

    added UT for object instantiation

---
+++ pkg/lang/objects_test.go
@@ -0,0 +1,16 @@
+package lang
+
+import (
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestObjectsInstantiate(t *testing.T) {
+	for _, obj := range Objects {
+		objInstance := obj.New()
+		structName := reflect.TypeOf(objInstance).Elem().Name()
+		assert.Contains(t, obj.Kind, strings.ToLower(structName), "%s instantiated to %s", structName, obj.Kind)
+	}
+}
