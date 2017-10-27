4ae318acf7d235bc20bca1d38d97a654eef0868e
ralekseenkov <ralekseenkov@gmail.com>
2017-10-26 23:56:49 -0700

    progress godoc

---
+++ pkg/engine/progress/doc.go
@@ -0,0 +1,3 @@
+// Package progress implements progress indicators, which are used during state enforcement while running actions and
+// plugins to estimate progress and show completion percentage to the user.
+package progress
