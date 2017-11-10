bd1168577bbabc665e898e6dec91f243fde1d053
Sergey Lukjanov <me@slukjanov.name>
2017-11-10 04:36:00 -0800

    Attach/detach actions should update corresponding component instance

--- pkg/engine/apply/action/component/dependency_attach.go
+++ pkg/engine/apply/action/component/dependency_attach.go
@@ -1,8 +1,10 @@
 package component
 
 import (
+	"fmt"
 	"github.com/Aptomi/aptomi/pkg/engine/apply/action"
 	"github.com/Aptomi/aptomi/pkg/runtime"
+	"time"
 )
 
 // AttachDependencyActionObject is an informational data structure with Kind and Constructor for the action
@@ -31,5 +33,19 @@ func NewAttachDependencyAction(revision runtime.Generation, componentKey string,
 
 // Apply applies the action
 func (a *AttachDependencyAction) Apply(context *action.Context) error {
+	return a.updateActualState(context)
+}
+
+func (a *AttachDependencyAction) updateActualState(context *action.Context) error {
+	// preserve previous creation date before overwriting
+	prevCreatedOn := context.ActualState.ComponentInstanceMap[a.ComponentKey].CreatedOn
+	instance := context.DesiredState.ComponentInstanceMap[a.ComponentKey]
+	instance.UpdateTimes(prevCreatedOn, time.Now())
+
+	context.ActualState.ComponentInstanceMap[a.ComponentKey] = instance
+	err := context.ActualStateUpdater.Save(instance)
+	if err != nil {
+		return fmt.Errorf("error while update actual state: %s", err)
+	}
 	return nil
 }
