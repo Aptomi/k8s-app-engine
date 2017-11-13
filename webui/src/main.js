3b538c58d2f1952266da371423f257e7c9693b76
ralekseenkov <ralekseenkov@gmail.com>
2017-11-13 15:19:03 -0800

    removed unnecessary pages & components from UI

--- webui/src/main.js
+++ webui/src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import './lib/css'
 import './lib/script'
-import './lib/global'
 
 import Vue from 'vue'
 import App from './App'
--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -7,9 +7,6 @@ import vbclass from 'vue-body-class'
 // Authentication library
 import auth from 'lib/auth'
 
-// Components
-import Sample from 'components/Sample.vue'
-
 // Aptomi pages
 import Login from 'pages/auth/Login.vue'
 import ShowObjects from 'pages/policy/ShowObjects.vue'
@@ -17,28 +14,6 @@ import BrowsePolicy from 'pages/policy/BrowsePolicy.vue'
 import ShowDependencies from 'pages/policy/ShowDependencies.vue'
 import ShowUserRoles from 'pages/policy/ShowUserRoles.vue'
 import ShowAuditLog from 'pages/policy/ShowAuditLog.vue'
-
-// Examples
-import DashboardV1 from 'examples/Dashboard.v1.vue'
-import DashboardV2 from 'examples/Dashboard.v2.vue'
-import InfoBoxExample from 'examples/InfoBoxExample'
-import ChartExample from 'examples/ChartExample'
-import AlertExample from 'examples/AlertExample'
-import ModalExample from 'examples/ModalExample'
-import WidgetsExample from 'examples/WidgetsExample'
-import APIExample from 'examples/APIExample'
-
-// UI Element Groups
-import General from 'pages/ui-elements/General.vue'
-import Icons from 'pages/ui-elements/Icons.vue'
-import Buttons from 'pages/ui-elements/Buttons.vue'
-import Sliders from 'pages/ui-elements/Sliders.vue'
-import Timeline from 'pages/ui-elements/Timeline.vue'
-import Modals from 'pages/ui-elements/Modals.vue'
-
-// forms
-import GeneralElements from 'pages/forms/GeneralElements.vue'
-import AdvancedElements from 'pages/forms/AdvancedElements.vue'
 
 Vue.use(Router)
 
@@ -74,91 +49,6 @@ const router = new Router({
       path: '/policy/audit',
       name: 'ShowAuditLog',
       component: ShowAuditLog
-    },
-    {
-      path: '/sample',
-      name: 'Sample',
-      component: Sample
-    },
-    {
-      path: '/dashboard/v1',
-      name: 'DashboardV1',
-      component: DashboardV1
-    },
-    {
-      path: '/dashboard/v2',
-      name: 'DashboardV2',
-      component: DashboardV2
-    },
-    {
-      path: '/examples/infobox',
-      name: 'InfoBoxExample',
-      component: InfoBoxExample
-    },
-    {
-      path: '/examples/chart',
-      name: 'ChartExample',
-      component: ChartExample
-    },
-    {
-      path: '/examples/alert',
-      name: 'AlertExample',
-      component: AlertExample
-    },
-    {
-      path: '/examples/modal',
-      name: 'ModalExample',
-      component: ModalExample
-    },
-    {
-      path: '/examples/widgets',
-      name: 'WidgetsExample',
-      component: WidgetsExample
-    },
-    {
-      path: '/examples/api-example',
-      name: 'APIExample',
-      component: APIExample
-    },
-    {
-      path: '/ui-elements/general',
-      name: 'General',
-      component: General
-    },
-    {
-      path: '/ui-elements/icons',
-      name: 'Icons',
-      component: Icons
-    },
-    {
-      path: '/ui-elements/buttons',
-      name: 'Buttons',
-      component: Buttons
-    },
-    {
-      path: '/ui-elements/sliders',
-      name: 'Sliders',
-      component: Sliders
-    },
-    {
-      path: '/ui-elements/timeline',
-      name: 'Timeline',
-      component: Timeline
-    },
-    {
-      path: '/ui-elements/modals',
-      name: 'Modals',
-      component: Modals
-    },
-    {
-      path: '/forms/general-elements',
-      name: 'GeneralElements',
-      component: GeneralElements
-    },
-    {
-      path: '/forms/advanced-elements',
-      name: 'AdvancedElements',
-      component: AdvancedElements
     },
     {
       path: '/login',
