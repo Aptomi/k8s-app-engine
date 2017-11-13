346f128cadeac5dff2890c77b45a00990ca48b09
Sergey Lukjanov <me@slukjanov.name>
2017-11-13 14:14:38 -0800

    Fix lint for retry util

--- pkg/util/retry/util.go
+++ pkg/util/retry/util.go
@@ -1,9 +1,13 @@
 package retry
 
-import "time"
+import (
+	"time"
+)
 
+// Func is the function to retry returning true if it's successfully completed
 type Func = func() bool
 
+// Do retries provided function "attempts" times with provided interval and returning true if it's successfully completed
 func Do(attempts int, interval time.Duration, f Func) bool {
 	for attempt := 0; ; attempt++ {
 		if f() {
