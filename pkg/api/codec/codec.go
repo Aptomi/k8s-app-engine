8d5bce6250a682ff643119ab2aa69cd5c50a62d0
Sergey Lukjanov <me@slukjanov.name>
2017-11-14 01:38:53 -0800

    Extend contentType.Write* to support slices

--- pkg/api/codec/codec.go
+++ pkg/api/codec/codec.go
@@ -94,7 +94,36 @@ func (handler *ContentTypeHandler) WriteOneWithStatus(writer http.ResponseWriter
 	if body != nil {
 		data, err := handler.GetCodec(request.Header).EncodeOne(body)
 		if err != nil {
-			panic(fmt.Sprintf("Error while encoding body of kind: %s", body.GetKind()))
+			panic(fmt.Sprintf("Error while encoding body of kind %s: %s", body.GetKind(), err))
+		}
+
+		_, wErr := fmt.Fprint(writer, string(data))
+		if wErr != nil {
+			panic(fmt.Sprintf("Error while writing body: %s", wErr))
+		}
+	}
+}
+
+// WriteMany runtime objects into the provided response writer using correct content type (taken from provided request)
+// with default http status (200 OK)
+func (handler *ContentTypeHandler) WriteMany(writer http.ResponseWriter, request *http.Request, body []runtime.Object) {
+	handler.WriteManyWithStatus(writer, request, body, http.StatusOK)
+}
+
+// WriteManyWithStatus runtime objects into the provided response writer using correct content type (taken from provided request)
+// with specified http status
+func (handler *ContentTypeHandler) WriteManyWithStatus(writer http.ResponseWriter, request *http.Request, body []runtime.Object, status int) {
+	writer.Header().Set("Content-Type", handler.GetContentType(request.Header))
+	writer.WriteHeader(status)
+
+	if body != nil {
+		data, err := handler.GetCodec(request.Header).EncodeMany(body)
+		if err != nil {
+			if len(body) > 0 {
+				panic(fmt.Sprintf("Error while encoding body of kind %s: %s", body[0].GetKind(), err))
+			} else {
+				panic(fmt.Sprintf("Error while encoding empty list: %s", err))
+			}
 		}
 
 		_, wErr := fmt.Fprint(writer, string(data))
