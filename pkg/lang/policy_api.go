6c3a7b745cc5bb236df587bf2ad27bd16b0f1b94
Sergey Lukjanov <me@slukjanov.name>
2017-11-15 00:23:51 -0800

    Add comments and fix APIPolicy/Namespace type

--- pkg/lang/policy_api.go
+++ pkg/lang/policy_api.go
@@ -1,9 +1,11 @@
 package lang
 
+// APIPolicy is a Policy representation for API filtered for specific user
 type APIPolicy struct {
-	Namespace map[string]*PolicyNamespace
+	Namespace map[string]*APIPolicyNamespace
 }
 
+// APIPolicyNamespace is a PolicyNamespace representation for API filtered for specific user
 type APIPolicyNamespace struct {
 	Services     map[string]*Service
 	Contracts    map[string]*Contract
@@ -13,6 +15,7 @@ type APIPolicyNamespace struct {
 	Dependencies map[string]*Dependency
 }
 
+// APIPolicy returns Policy representation for API filtered for specific user
 func (view *PolicyView) APIPolicy() *APIPolicy {
 	// if we're changing data in any map, we should copy map as well
 	// don't change existing object, make copy of them
