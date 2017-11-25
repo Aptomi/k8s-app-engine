1309e7b9492d9a783287e327f592860a5d3f1ae9
Sergey Lukjanov <me@slukjanov.name>
2017-11-25 11:26:48 -1000

    Remove no more used EnsureSingleFile helper

--- pkg/util/file.go
+++ pkg/util/file.go
@@ -20,14 +20,3 @@ func WriteTempFile(prefix string, content string) string {
 
 	return tmpFile.Name()
 }
-
-// EnsureSingleFile ensures that only one file matches the list of files
-func EnsureSingleFile(files []string) (string, error) {
-	if len(files) <= 0 {
-		return "", fmt.Errorf("no files found")
-	}
-	if len(files) > 1 {
-		return "", fmt.Errorf("more than one file found")
-	}
-	return files[0], nil
-}
