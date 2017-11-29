9e61d476deead93526671b432337494d023c9d8d
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-11-28 18:08:46 -0800

    better code coverage for multi-source user loader

--- pkg/external/users/user_loader_multiple_sources_test.go
+++ pkg/external/users/user_loader_multiple_sources_test.go
@@ -23,11 +23,20 @@ func TestUserLoaderFromMultipleSources(t *testing.T) {
 	u2 := makeUserLoader(len(u1.LoadUsersAll().Users), 15)
 	unitTests := NewUserLoaderFromFile("../../testdata/unittests/users.yaml", make(map[string]bool))
 	uMulti := NewUserLoaderMultipleSources([]UserLoader{u1, u2, unitTests})
+
+	// Test that LoadUsersAll() works correctly
 	assert.Equal(t, 28, len(uMulti.LoadUsersAll().Users), "Correct number of users should be loaded")
 
-	names := []string{"Alice", "Bob", "Carol"}
+	// Test that LoadUserByName() works correctly
+	alice := uMulti.LoadUserByName("aLICe")
+	assert.NotNil(t, alice, "Alice should be loaded by name")
+	assert.Equal(t, "Alice", alice.Name, "Alice should be loaded by name")
 
-	// test that authentication works
+	nonExisting := uMulti.LoadUserByName("not-existing")
+	assert.Nil(t, nonExisting, "Non-existing user should be nil when loaded ny name")
+
+	// Test that Authenticate() works correctly
+	names := []string{"Alice", "Bob", "Carol"}
 	for _, name := range names {
 		// successful authentication (password == lowercase name for test users, e.g. 'alice')
 		{
@@ -35,11 +44,17 @@ func TestUserLoaderFromMultipleSources(t *testing.T) {
 			assert.NoError(t, err, "Authentication should be successful")
 			assert.NotEmpty(t, user, "User should be returned as a result of authentication")
 		}
-		// failed authentication
+		// failed authentication (existing user, incorrect password)
 		{
 			user, err := uMulti.Authenticate(name, name+"pass")
 			assert.Error(t, err, "Authentication should not be successful")
 			assert.Empty(t, user, "User should not be returned as a result of failed authentication")
 		}
+	}
+	// failed authentication with non-existing user
+	{
+		user, err := uMulti.Authenticate("non-existing", "non-existing")
+		assert.Error(t, err, "Authentication should not be successful")
+		assert.Empty(t, user, "User should not be returned as a result of failed authentication")
 	}
 }
