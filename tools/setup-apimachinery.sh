6fc57633807fd60ed9bc9662d03b7f0e045531bb
Sergey Lukjanov <me@slukjanov.name>
2017-12-03 13:48:27 -0800

    Remove bin directory after compiling k8s parts as it only containts temp binary

--- tools/setup-apimachinery.sh
+++ tools/setup-apimachinery.sh
@@ -25,3 +25,5 @@ set -euo pipefail
 rm -rf ./vendor/k8s.io/{kube-aggregator,apiserver,apimachinery,client-go,metrics}
 
 cp -r ./vendor/k8s.io/kubernetes/staging/src/k8s.io/{kube-aggregator,apiserver,apimachinery,client-go,metrics} ./vendor/k8s.io
+
+rm -rf bin
