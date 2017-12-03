b3f4867dc39f2ac91ad9ee0d82b2527fb0f3c0cc
Sergey Lukjanov <me@slukjanov.name>
2017-12-03 13:06:28 -0800

    Add API check to make sure that there are no duplicate objects in policy

--- pkg/api/util.go
+++ pkg/api/util.go
@@ -3,18 +3,26 @@ package api
 import (
 	"fmt"
 	"github.com/Aptomi/aptomi/pkg/lang"
+	"github.com/Aptomi/aptomi/pkg/runtime"
 	"net/http"
 )
 
 func (api *coreAPI) readLang(request *http.Request) []lang.Base {
 	result := make([]lang.Base, 0)
 
+	exists := make(map[string]bool, len(result))
 	for _, obj := range api.contentType.Read(request) {
 		langObj, ok := obj.(lang.Base)
 
 		if !ok {
 			panic(fmt.Sprintf("Trying to read lang objects while non-lang ones found: %s", obj.GetKind()))
 		}
+
+		key := runtime.KeyForStorable(langObj)
+		if exists[key] {
+			panic(fmt.Sprintf("Duplicate objects with key %s detected in the request", key))
+		}
+		exists[key] = true
 
 		result = append(result, langObj)
 	}
