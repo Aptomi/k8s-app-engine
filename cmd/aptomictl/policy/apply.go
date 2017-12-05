94cd4a08f6b1cfc40253231dbc9790da97e0854c
Sergey Lukjanov <me@slukjanov.name>
2017-12-05 01:41:29 -0800

    Start using cli format in policy apply

--- cmd/aptomictl/policy/apply.go
+++ cmd/aptomictl/policy/apply.go
@@ -2,6 +2,7 @@ package policy
 
 import (
 	"fmt"
+	"github.com/Aptomi/aptomi/cmd/common"
 	"github.com/Aptomi/aptomi/pkg/client/rest"
 	"github.com/Aptomi/aptomi/pkg/client/rest/http"
 	"github.com/Aptomi/aptomi/pkg/config"
@@ -33,7 +34,13 @@ func newApplyCommand(cfg *config.Client) *cobra.Command {
 			}
 
 			// todo(slukjanov): replace with -o yaml / json / etc handler
-			fmt.Println(result)
+			data, err := common.Format(cfg, false, result)
+			if err != nil {
+				panic(fmt.Sprintf("Error while formating policy update result: %s", err))
+			}
+
+			// print result to stdout
+			fmt.Println(string(data))
 
 			if !wait {
 				return
