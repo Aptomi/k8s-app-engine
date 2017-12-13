53418655705f469df7669b1de4138828dbb97633
Roman Alekseenkov <ralekseenkov@gmail.com>
2017-12-13 11:14:22 +0300

    client can handle wildcards in file names

--- cmd/aptomictl/policy/common.go
+++ cmd/aptomictl/policy/common.go
@@ -74,30 +74,36 @@ func findPolicyFiles(policyPaths []string) ([]string, error) {
 			return nil, fmt.Errorf("error reading filepath: %s", errPath)
 		}
 
-		if stat, err := os.Stat(policyPath); err == nil {
-			if stat.IsDir() { // if dir provided, use all yaml files from it
-				files, errGlob := zglob.Glob(filepath.Join(policyPath, "**", "*.yaml"))
-				if errGlob != nil {
-					return nil, fmt.Errorf("error while searching yaml files in directory: %s error: %s", policyPath, err)
-				}
-				allFiles = append(allFiles, files...)
-			} else { // if specific file provided, use it
-				allFiles = append(allFiles, policyPath)
+		// if it's a directory, use all yaml files from it
+		if stat, err := os.Stat(policyPath); err == nil && stat.IsDir() {
+			// if dir provided, use all yaml files from it
+			files, errGlob := zglob.Glob(filepath.Join(policyPath, "**", "*.yaml"))
+			if errGlob != nil {
+				return nil, fmt.Errorf("error while searching yaml files in '%s' (error: %s)", policyPath, err)
 			}
-		} else if os.IsNotExist(err) {
-			return nil, fmt.Errorf("path doesn't exist: %s error: %s", policyPath, err)
-		} else {
-			return nil, fmt.Errorf("error while processing path: %s", err)
+			allFiles = append(allFiles, files...)
+			continue
 		}
+
+		// otherwise, try as a single file or glob pattern/mask (so we can feed wildcard mask and process multiple files)
+		files, errGlob := zglob.Glob(policyPath)
+		if errGlob != nil {
+			return nil, fmt.Errorf("error while searching yaml files in '%s' (error: %s)", policyPath, errGlob)
+		}
+		if len(files) > 0 {
+			allFiles = append(allFiles, files...)
+			continue
+		}
+
+		return nil, fmt.Errorf("path doesn't exist or no YAML files found under: %s")
 	}
 
 	sort.Strings(allFiles)
 
-	// todo(slukjanov): log list of files from which we're applying policy
-	//fmt.Println("Apply policy from following files:")
-	//for idx, policyPath := range allFiles {
-	//	fmt.Println(idx, "-", policyPath)
-	//}
+	fmt.Println("Applying policy from:")
+	for _, policyPath := range allFiles {
+		fmt.Println("  [*] " + policyPath)
+	}
 
 	return allFiles, nil
 }
