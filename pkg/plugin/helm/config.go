3b6e0597e0fd8b89de162a867ecc7c348339eb65
Sergey Lukjanov <me@slukjanov.name>
2018-01-19 18:05:41 -0800

    Add support for local k8s cluster

--- pkg/plugin/helm/config.go
+++ pkg/plugin/helm/config.go
@@ -3,16 +3,14 @@ package helm
 import (
 	"fmt"
 	"github.com/Aptomi/aptomi/pkg/lang"
-	"github.com/Aptomi/aptomi/pkg/util"
-	"gopkg.in/yaml.v2"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 // Config represents K8s/Helm plugin configuration
 type Config struct {
-	Context         string
 	Namespace       string
 	TillerNamespace string
+	Local           bool
+	Context         string
 	KubeConfig      interface{} // it's just a kubeconfig, we don't need to parse it
 }
 
@@ -27,43 +25,23 @@ func (cache *clusterCache) initConfig(cluster *lang.Cluster) error {
 		return fmt.Errorf("error while parsing Helm plugin specific cluster config: %s", err)
 	}
 
-	data, err := yaml.Marshal(config.KubeConfig)
+	if config.Local && config.KubeConfig != nil {
+		return fmt.Errorf("kube-config can't be specified when using local type in cluster: %s", cluster.Name)
+	}
+
+	if config.KubeConfig != nil {
+		cache.kubeConfig, cache.namespace, err = initKubeConfig(config, cluster)
+	} else {
+		cache.kubeConfig, err = initLocalKubeConfig()
+	}
 	if err != nil {
-		return fmt.Errorf("error while marshaling kube config into bytes: %s", err)
+		return err
 	}
-
-	// todo make sure temp file removed after kube config created
-	kubeConfigFile := util.WriteTempFile(fmt.Sprintf("cluster-config"), data)
-
-	rules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfigFile}
-
-	overrides := &clientcmd.ConfigOverrides{}
-	if len(config.Context) > 0 {
-		overrides.CurrentContext = config.Context
-	}
-
-	conf := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, overrides)
-
-	rawConf, err := conf.RawConfig()
-	if err != nil {
-		return fmt.Errorf("error while getting raw kube config for cluster %s: %s", cluster.Name, err)
-	}
-
-	if len(config.Context) == 0 && len(rawConf.CurrentContext) == 0 {
-		return fmt.Errorf("context for cluster %s should be explicitly defined (context in cluster config or current-context in kubeconfig)", cluster.Name)
-	}
-
-	clientConf, err := conf.ClientConfig()
-	if err != nil {
-		return fmt.Errorf("could not get kubernetes config for cluster %s: %s", cache.cluster.Name, err)
-	}
-	cache.kubeConfig = clientConf
 
 	if len(config.Namespace) > 0 {
 		cache.namespace = config.Namespace
-	} else if namespace, _, nsErr := conf.Namespace(); nsErr == nil && len(namespace) > 0 {
-		cache.namespace = namespace
-	} else {
+	}
+	if len(cache.namespace) == 0 {
 		cache.namespace = "default"
 	}
 
--- pkg/plugin/helm/k8s_util.go
+++ pkg/plugin/helm/k8s_util.go
@@ -2,12 +2,60 @@ package helm
 
 import (
 	"fmt"
+	"github.com/Aptomi/aptomi/pkg/lang"
+	"github.com/Aptomi/aptomi/pkg/util"
+	"gopkg.in/yaml.v2"
 	"k8s.io/apimachinery/pkg/api/errors"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	api "k8s.io/client-go/pkg/api/v1"
 	rbacapi "k8s.io/client-go/pkg/apis/rbac/v1beta1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
+
+func initKubeConfig(config *Config, cluster *lang.Cluster) (*rest.Config, string, error) {
+	data, err := yaml.Marshal(config.KubeConfig)
+	if err != nil {
+		return nil, "", fmt.Errorf("error while marshaling kube config into bytes: %s", err)
+	}
+
+	// todo make sure temp file removed after kube config created
+	kubeConfigFile := util.WriteTempFile(fmt.Sprintf("cluster-config"), data)
+
+	rules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeConfigFile}
+
+	overrides := &clientcmd.ConfigOverrides{}
+	if len(config.Context) > 0 {
+		overrides.CurrentContext = config.Context
+	}
+
+	conf := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, overrides)
+
+	rawConf, err := conf.RawConfig()
+	if err != nil {
+		return nil, "", fmt.Errorf("error while getting raw kube config for cluster %s: %s", cluster.Name, err)
+	}
+
+	if len(config.Context) == 0 && len(rawConf.CurrentContext) == 0 {
+		return nil, "", fmt.Errorf("context for cluster %s should be explicitly defined (context in cluster config or current-context in kubeconfig)", cluster.Name)
+	}
+
+	clientConf, err := conf.ClientConfig()
+	if err != nil {
+		return nil, "", fmt.Errorf("could not get kubernetes config for cluster %s: %s", cluster.Name, err)
+	}
+
+	if namespace, _, nsErr := conf.Namespace(); nsErr == nil && len(namespace) > 0 {
+		return clientConf, namespace, nil
+	}
+
+	return clientConf, "", nil
+}
+
+func initLocalKubeConfig() (*rest.Config, error) {
+	return rest.InClusterConfig()
+}
 
 func (cache *clusterCache) newKubeClient() (kubernetes.Interface, error) {
 	client, err := kubernetes.NewForConfig(cache.kubeConfig)
