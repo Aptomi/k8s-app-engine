cdd7b37cc02219a1e0352f7f5c8ae949acedf275
Sergey Lukjanov <me@slukjanov.name>
2018-02-02 12:30:15 -0800

    Add script for testing install/uninstall scripts

---
+++ tools/test-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -exou pipefail
+
+rm -rf $(PWD)/.aptomi-install-cache
+
+function test_install {
+    os="$1"
+    cmd="$2"
+
+    docker run -it --rm -p 27866:27866 \
+                    -v $(PWD)/.aptomi-install-cache:/root/.aptomi-install-cache \
+                    -v $(PWD)/scripts:/scripts \
+                    -w /scripts \
+                    aptomi/aptomi-test-install:${os} \
+                    sh -c "${cmd}"
+}
+
+test_install xenial "./aptomi_install.sh && ./aptomi_uninstall_and_clean.sh"
+test_install xenial "./aptomi_install.sh --with-example && ./aptomi_uninstall_and_clean.sh"
+
+test_install centos7 "./aptomi_install.sh && ./aptomi_uninstall_and_clean.sh"
+test_install centos7 "./aptomi_install.sh --with-example && ./aptomi_uninstall_and_clean.sh"
+
