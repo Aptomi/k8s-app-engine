fe1414c4fbaf5b6a02a0429d71d763e9a9024e3b
Sergey Lukjanov <me@slukjanov.name>
2018-02-02 02:21:07 -0800

    Ensure that helm plugin implements all needed interfaces

--- pkg/plugin/helm/plugin.go
+++ pkg/plugin/helm/plugin.go
@@ -2,6 +2,7 @@ package helm
 
 import (
 	"github.com/Aptomi/aptomi/pkg/config"
+	"github.com/Aptomi/aptomi/pkg/plugin"
 	"sync"
 )
 
@@ -10,6 +11,10 @@ type Plugin struct {
 	cache *sync.Map
 	cfg   config.Helm
 }
+
+var _ plugin.Plugin = &Plugin{}
+var _ plugin.DeployPlugin = &Plugin{}
+var _ plugin.PostProcessPlugin = &Plugin{}
 
 // NewPlugin creates a new helm plugin
 func NewPlugin(cfg config.Helm) *Plugin {
