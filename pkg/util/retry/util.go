c641bba3196a7dc345fea05036d5ca9c2f95aadb
Sergey Lukjanov <me@slukjanov.name>
2018-02-02 01:51:40 -0800

    Make retry.Do panic if it's started with interval less then 1/10 second

--- pkg/util/retry/util.go
+++ pkg/util/retry/util.go
@@ -1,6 +1,7 @@
 package retry
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -9,6 +10,10 @@ type Func = func() bool
 
 // Do retries provided function "attempts" times with provided interval and returning true if it's successfully completed
 func Do(attempts int, interval time.Duration, f Func) bool {
+	if interval < 1*time.Second/10 {
+		panic(fmt.Sprintf("retry.Do used with interval less then 1/10 second, it seems dangerous: %s", interval))
+	}
+
 	for attempt := 0; ; attempt++ {
 		if f() {
 			return true
