69787879a8cda799e9317a4ce3d0daea5678d897
Sergey Lukjanov <me@slukjanov.name>
2018-02-18 04:19:42 -0800

    Add k8s plugin config

--- pkg/config/plugins.go
+++ pkg/config/plugins.go
@@ -4,7 +4,13 @@ import "time"
 
 // Plugins represents configs for all plugins
 type Plugins struct {
+	Kube Kube
 	Helm Helm
+}
+
+// Kube represents config for Kubernetes plugin
+type Kube struct {
+	Timeout time.Duration
 }
 
 // Helm represents configs for Helm plugin
