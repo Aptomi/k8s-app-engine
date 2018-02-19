f048e99335d61666dbcafa235b342576035b159b
Sergey Lukjanov <me@slukjanov.name>
2018-02-19 10:53:38 -0800

    Better package doc for plugins

--- pkg/plugin/doc.go
+++ pkg/plugin/doc.go
@@ -1,4 +1,13 @@
-// Package plugin introduces plugins for Aptomi engine, which are responsible for component instantiation
-// and management on different cloud providers (e.g. deployment plugin, such as k8s/helm) and also configuration of
-// underlying cloud infrastructure components (e.g. istio plugin).
+// Package plugin introduces plugins for Aptomi engine such as cluster (kubernetes) and code (helm) plugins.
+//
+// Cluster plugins responsible for working with different cloud providers, such as kubernetes. Separated instance of
+// the cluster plugins is created for each lang.Cluster, so, it could safely cache data.
+//
+// Code plugins responsible for handling deployment into the cloud providers, such as helm. Separated instance of the
+// code plugin is created for each pair of the lang.Cluster and code type, for example, cluster "test-1" and code
+// type "helm".
+//
+// PostProcess plugins has access to all data across all cloud providers and executed as a last step.
+//
+// All plugins created for single enforcement cycle or API call using plugin registry.
 package plugin
