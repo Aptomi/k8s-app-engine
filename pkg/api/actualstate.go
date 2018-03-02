81a41e55c6ffe66ef353e8e16bd3fb03c341d453
Sergey Lukjanov <me@slukjanov.name>
2018-03-02 01:21:26 -0800

    Trigger enforcement after state reset

--- pkg/api/actualstate.go
+++ pkg/api/actualstate.go
@@ -12,5 +12,8 @@ func (api *coreAPI) handleActualStateReset(writer http.ResponseWriter, request *
 		panic(fmt.Sprintf("error while resetting actual state: %s", err))
 	}
 
+	// signal to the channel that policy has changed, that will trigger the enforcement right away
+	api.policyChanged <- true
+
 	api.handleRevisionGet(writer, request, params)
 }
