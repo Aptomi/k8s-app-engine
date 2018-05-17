d12181fd438c2d00f835b2f30132099a36d43d2a
ralekseenkov <ralekseenkov@gmail.com>
2018-05-17 01:08:07 -0700

    removed the old method for getting dependency status

--- pkg/api/dependency.go
+++ pkg/api/dependency.go
@@ -164,59 +164,6 @@ func fetchEndpointsForDependencies(result *DependencyStatus, actualState *resolv
 }
 
 /*
-type dependencyStatusWrapper struct {
-	Data interface{}
-}
-
-func (g *dependencyStatusWrapper) GetKind() string {
-	return "dependencyStatus"
-}
-
-func (api *coreAPI) handleDependencyStatusGet(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	gen := runtime.LastGen
-	policy, _, err := api.store.GetPolicy(gen)
-	if err != nil {
-		panic(fmt.Sprintf("error while getting requested policy: %s", err))
-	}
-
-	ns := params.ByName("ns")
-	kind := lang.DependencyObject.Kind
-	name := params.ByName("name")
-
-	obj, err := policy.GetObject(kind, name, ns)
-	if err != nil {
-		panic(fmt.Sprintf("error while getting object %s/%s/%s in policy #%s", ns, kind, name, gen))
-	}
-	if obj == nil {
-		api.contentType.WriteOneWithStatus(writer, request, nil, http.StatusNotFound)
-	}
-
-	// once dependency is loaded, we need to find its state in the actual state
-	dependency := obj.(*lang.Dependency)
-	actualState, err := api.store.GetActualState()
-	if err != nil {
-		panic(fmt.Sprintf("Can't load actual state to get endpoints: %s", err))
-	}
-
-	var status string
-	depKey := runtime.KeyForStorable(dependency)
-
-	foundRefs := false
-	for _, instance := range actualState.ComponentInstanceMap {
-		if _, ok := instance.DependencyKeys[depKey]; ok {
-			foundRefs = true
-			break
-		}
-	}
-	if foundRefs {
-		status = "Active"
-	} else {
-		status = "Inactive"
-	}
-
-	api.contentType.WriteOne(writer, request, &dependencyStatusWrapper{Data: status})
-}
-
 type dependencyResourcesWrapper struct {
 	Resources plugin.Resources
 }
