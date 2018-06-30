ab4f3949d44b85f5037a55f5aa300b6ce2d712bb
Sergey Lukjanov <me@slukjanov.name>
2018-06-30 16:27:30 -0700

    index_test fixed with lastest changes

--- pkg/runtime/store/index_test.go
+++ pkg/runtime/store/index_test.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/Aptomi/aptomi/pkg/engine"
 	"github.com/Aptomi/aptomi/pkg/runtime"
+	"github.com/Aptomi/aptomi/pkg/runtime/store"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestIndexes(t *testing.T) {
-	indexes := store.Indexes(engine.TypeRevision)
+	indexes := store.IndexesFor(engine.TypeRevision)
 	assert.NotNil(t, indexes)
 	assert.Len(t, indexes, 3)
 	assert.Contains(t, indexes, "PolicyGen")
@@ -21,9 +22,9 @@ func TestIndexes(t *testing.T) {
 		},
 		PolicyGen: 42,
 	}
-	assert.Equal(t, "system/revision@PolicyGen@42", indexes["PolicyGen"].KeyForStorable(revision, NewJsonCodec()))
-	assert.Equal(t, "system/revision@Status@some_status", indexes["Status"].KeyForStorable(revision, NewJsonCodec()))
-	assert.Equal(t, "system/revision", indexes[""].KeyForStorable(revision, NewJsonCodec()))
+	assert.Equal(t, "system/revision@PolicyGen@42", indexes.KeyForStorable("PolicyGen", revision, store.NewJsonCodec()))
+	assert.Equal(t, "system/revision@Status@some_status", indexes.KeyForStorable("Status", revision, store.NewJsonCodec()))
+	assert.Equal(t, "system/revision", indexes.KeyForStorable(store.LastGenIndex, revision, store.NewJsonCodec()))
 
-	assert.Equal(t, "system/revision@PolicyGen@42", indexes["PolicyGen"].KeyForValue(engine.RevisionKey, 42, NewJsonCodec()))
+	assert.Equal(t, "system/revision@PolicyGen@42", indexes.KeyForValue("PolicyGen", engine.RevisionKey, 42, store.NewJsonCodec()))
 }
