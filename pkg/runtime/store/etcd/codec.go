75f0ab5f54e32b7760b906216554f66c1cbb8d84
Sergey Lukjanov <me@slukjanov.name>
2018-06-30 16:27:52 -0700

    etcd codec helper extended to support generations

--- pkg/runtime/store/etcd/codec.go
+++ pkg/runtime/store/etcd/codec.go
@@ -1,13 +1,16 @@
 package etcd
 
 import (
+	"encoding/binary"
 	"fmt"
+
+	"github.com/Aptomi/aptomi/pkg/runtime"
 )
 
 func (s *etcdStore) marshal(value interface{}) []byte {
 	data, err := s.codec.Marshal(value)
 	if err != nil {
-		panic(fmt.Sprintf("unable to marshal value %v with error: %s", value, err))
+		panic(fmt.Sprintf("error while marshaling value %v with error: %s", value, err))
 	}
 
 	return data
@@ -15,6 +18,17 @@ func (s *etcdStore) marshal(value interface{}) []byte {
 
 func (s *etcdStore) unmarshal(data []byte, value interface{}) {
 	if err := s.codec.Unmarshal(data, value); err != nil {
-		panic(fmt.Sprintf("error while unmarshalling data: %s", err))
+		panic(fmt.Sprintf("error while unmarshaling data: %s", err))
 	}
+}
+
+func (s *etcdStore) marshalGen(generation runtime.Generation) string {
+	data := make([]byte, 8)
+	binary.BigEndian.PutUint64(data, uint64(generation))
+
+	return string(data)
+}
+
+func (s *etcdStore) unmarshalGen(data string) runtime.Generation {
+	return runtime.Generation(binary.BigEndian.Uint64([]byte(data)))
 }
