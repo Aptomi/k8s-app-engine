cece0b7e8f1da9fea85097f7fb56f066a85fb1d9
Sergey Lukjanov <me@slukjanov.name>
2018-07-12 18:42:20 -1000

    Add tools/with_etcd.sh helper for tests and dev

---
+++ tools/with_etcd.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+function util::free_port() {
+    for port in $(seq 10000 11000); do
+        echo -ne "\035" | telnet 127.0.0.1 $port > /dev/null 2>&1;
+        [ $? -eq 1 ] && echo "$port" && break;
+    done
+}
+
+function etcd::start() {
+    etcd::stop
+    export ETCD_PORT=$(util::free_port)
+    docker run --name aptomi-etcd-smoke -d -p ${ETCD_PORT}:2379 quay.io/coreos/etcd /usr/local/bin/etcd --advertise-client-urls http://0.0.0.0:${ETCD_PORT} --listen-client-urls http://0.0.0.0:2379
+    echo "etcd listening on port ${ETCD_PORT}"
+    sleep 1
+}
+
+function etcd::stop() {
+    docker rm -f aptomi-etcd-smoke &>/dev/null || true
+}
+
+function cleanup() {
+    set +x
+    etcd::stop
+}
+
+trap cleanup EXIT
+
+etcd::start
+
+export APTOMI_DB_ENDPOINTS=127.0.0.1:${ETCD_PORT}
+export APTOMI_TEST_DB_ENDPOINTS=${APTOMI_DB_ENDPOINTS}
+
+set -x
+$@
+set +x
